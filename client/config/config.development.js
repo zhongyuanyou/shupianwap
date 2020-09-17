29ecfa303eafc114872c75cee31565fa14d36dc9
赵冬明 <zhaodongming@dgg.net>
2020-09-17 19:20:09 +0800

    feat:配置修改

--- client/config/config.development.js
+++ client/config/config.development.js
@@ -1,7 +1,7 @@
 /* T环境配置文件 */
 
 module.exports = {
-  baseURL: 'https://tmicrouag.dgg188.cn/', // 中间件
+  baseURL: 'https://tmicrouag.dgg188.cn/CHIPS-WAP/', // 中间件
   mchMerchantSgin: '62070A76D7BF904888B75450D2D6B4C4',
   mchCoding: '20190620000111',
 }
