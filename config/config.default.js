8549bb36cd0c246f82d2ef4b1c505d7a5d0692ff
赵冬明 <zhaodongming@dgg.net>
2020-09-18 14:03:45 +0800

    fix:修改活动ip

--- config/config.default.js
+++ config/config.default.js
@@ -34,7 +34,6 @@ module.exports = appInfo => {
   config.cluster = {
     listen: {
       port: 7001,
-      hostname: '127.0.0.1', // 不建议设置 hostname 为 '0.0.0.0'，它将允许来自外部网络和来源的连接，请在知晓风险的情况下使用
     },
   };
   // 在此处添加中间件配置
