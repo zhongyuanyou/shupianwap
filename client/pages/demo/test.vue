9e46fb0c57449728c35e8b0efa57c927a1f5d11c
maliang <maliang1@dgg.net>
2020-11-19 14:52:04 +0800

    feat: 新增组件开发分支

---
+++ client/pages/demo/test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h5>组件测试</h5>
+  </div>
+</template>
+
+<script>
+import Http from '~/api/eureka'
+export default {
+  name: 'Test',
+  components: {},
+  methods: {},
+}
+</script>
+
+<style scoped></style>
