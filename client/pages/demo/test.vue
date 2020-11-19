dee485b66be44c36a45c86314f982e799061543c
maliang <maliang1@dgg.net>
2020-11-19 15:28:25 +0800

    fix: 修改rem适配

--- client/pages/demo/test.vue
+++ client/pages/demo/test.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <h5>组件测试</h5>
+  <div class="demo-page">
+    <div class="modle-item"><h5>组件测试</h5></div>
   </div>
 </template>
 
@@ -13,4 +13,13 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style lang="less" scoped>
+.demo-page {
+  width: 100%;
+  .modle-item {
+    width: 100%;
+    padding: 0 40px;
+    box-sizing: border-box;
+  }
+}
+</style>
