2aea5066b65462b3a02e5c09aba8829b1159bc31
maliang <maliang1@dgg.net>
2020-11-24 13:36:06 +0800

    feat: 首页swiper布局完成

--- client/pages/home.vue
+++ client/pages/home.vue
@@ -2,6 +2,7 @@
   <div class="page-content">
     <search-banner></search-banner>
     <home-nav></home-nav>
+    <SwiperBanner></SwiperBanner>
     <Help></Help>
     <div style="height: 3000px">占位</div>
   </div>
@@ -10,12 +11,14 @@
 <script>
 import SearchBanner from '@/components/home/SearchBanner'
 import HomeNav from '@/components/home/HomeNav'
+import SwiperBanner from '@/components/home/SwiperBanner'
 import Help from '@/components/home/Help'
 export default {
   name: 'Home',
   components: {
     SearchBanner,
     HomeNav,
+    SwiperBanner,
     Help,
   },
   data() {
