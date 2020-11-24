05c85be3a7b87414dec7e83f8e55112529ab9e9a
蒲聪德 <pucongde@dgg.net>
2020-11-24 11:48:29 +0800

    feat:添加公共的手机，密码，验证码的校验

--- client/utils/check.js
+++ client/utils/check.js
@@ -46,3 +46,35 @@ export function checkEmail(email) {
   const Regex = /^[a-zA-Z0-9_-]+@((qq|163|sina|sohu|gmail|msn|live)+\.)+(com|cn|net|org)$/
   return Regex.test(email)
 }
+
+/**
+ * 校验手机号码
+ * @params {string} telephoneNumber 手机号码
+ * @returns  boolean
+ */
+export function checkPhone(telephoneNumber) {
+  const Regex = /^(((13[0-9]{1})|(15[0-9]{1})|(16[0-9]{1})|(17[3-8]{1})|(18[0-9]{1})|(19[0-9]{1})|(14[5-7]{1}))+\d{8})$/
+  return Regex.test(telephoneNumber)
+}
+
+/**
+ * 校验密码
+ * @params {string} password 密码
+ * @returns  boolean
+ */
+export function checkPassword(password) {
+  // 至少6-15个字符，至少1个大写字母，1个小写字母和1个数字，其他可以是任意字符
+  const Regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[^]{6,15}$/
+  return Regex.test(password)
+}
+
+/**
+ * 校验验证码
+ * @params {string} authCode 密码
+ * @returns  boolean
+ */
+export function checkAuthCode(authCode) {
+  // 6个数字
+  const Regex = /\d{6}/
+  return Regex.test(authCode)
+}
