1b6296e8e72b8f960b1ed707d348685afca95e19
xiashuiping <xiashuiping@dgg.net>
2020-11-25 11:03:24 +0800

    'feat:修改合作页面'

--- client/pages/my/cooperation/index.vue
+++ client/pages/my/cooperation/index.vue
@@ -15,7 +15,7 @@
     </div>
     <div class="cooperation-form">
       <div class="cooperation-form-title">合作信息填写</div>
-      <sp-form label-width="80">
+      <sp-form label-width="80" @submit="submit">
         <sp-field
           v-model="form.contactsName"
           name="联系人"
@@ -34,30 +34,49 @@
           v-model="form.position"
           name="您的职位"
           label="您的职位"
-          placeholder="请输入您的职位"
-          :rules="[{ required: true, message: '请输入您的职位' }]"
-        />
+          :readonly="true"
+          :clickable="true"
+          placeholder="请选择您的职位"
+          :rules="[
+            { required: true, message: '请选择您的职位', trigger: 'onChange' },
+          ]"
+          @click="selectPosition"
+        >
+          <template #button>
+            <sp-icon name="arrow" color="#CCCCCC"></sp-icon>
+          </template>
+        </sp-field>
         <sp-field
           v-model="form.companyAddress"
           name="公司地址"
           label="公司地址"
           placeholder="请输入公司地址"
-          :rules="[{ required: true, message: '请输入公司地址' }]"
         />
         <sp-field
           v-model="form.phone"
           center
           clearable
+          type="tel"
           label="联系方式"
           placeholder="请输入联系方式"
+          :rules="[
+            {
+              validator: checkTel,
+              message: '请输入正确的手机号',
+              trigger: 'onChange',
+            },
+          ]"
         >
           <template #button>
             <sp-button
+              native-type="button"
               class="cooperation-form-button"
+              :disabled="!correctPhone"
               size="small"
               plain
               type="primary"
-              >获取验证码</sp-button
+              @click="getPhoneCode"
+              >{{ btnText }}</sp-button
             >
           </template>
         </sp-field>
@@ -66,8 +85,15 @@
           name=""
           label=" "
           placeholder="请输入验证码"
-          :rules="[{ required: true, message: '请输入验证码' }]"
+          :rules="[{ validator: checkCode, message: '请输入正确的验证码' }]"
         />
+        <sp-bottombar safe-area-inset-bottom>
+          <sp-bottombar-button
+            type="primary"
+            text="立即申请"
+            native-type="submit"
+          />
+        </sp-bottombar>
       </sp-form>
     </div>
     <div class="cooperation-introduce">
@@ -89,9 +115,15 @@
         <p class="cooperation-introduce-item-desc">{{ item.desc }}</p>
       </div>
     </div>
-    <sp-bottombar safe-area-inset-bottom>
-      <sp-bottombar-button type="primary" text="立即申请" @click="submit" />
-    </sp-bottombar>
+    <sp-popup v-model="showPop" position="bottom" :style="{ height: '318' }">
+      <sp-picker
+        title="职位选择"
+        show-toolbar
+        :columns="columns"
+        @confirm="onConfirm"
+        @cancel="onCancel"
+      />
+    </sp-popup>
   </div>
 </template>
 <script>
@@ -105,7 +137,11 @@ import {
   Field,
   Sticky,
   Image,
+  Icon,
+  Popup,
+  Picker,
 } from '@chipspc/vant-dgg'
+import { checkPhone, checkAuthCode, getCode } from '@/utils/check.js'
 export default {
   name: 'Cooperation',
   components: {
@@ -117,9 +153,14 @@ export default {
     [Field.name]: Field,
     [Sticky.name]: Sticky,
     [Image.name]: Image,
+    [Icon.name]: Icon,
+    [Picker.name]: Picker,
+    [Popup.name]: Popup,
   },
   data() {
     return {
+      btnText: '获取验证码',
+      timer: null,
       form: {
         contactsName: '',
         companyName: '',
@@ -145,13 +186,52 @@ export default {
           desc: '政策咨询，资质估价，专业风险评估',
         },
       ],
+      correctPhone: false,
+      showPop: false, // 下拉选择职位
+      columns: [
+        '某某职位',
+        '某某职位',
+        '某某职位',
+        '某某职位',
+        '某某职位',
+        '某某职位',
+        '某某职位',
+        '某某职位',
+      ],
     }
   },
   methods: {
     back() {
       this.$router.back()
     },
-    submit() {},
+    // 校验手机号
+    checkTel(val) {
+      this.correctPhone = checkPhone(val)
+      return this.correctPhone
+    },
+    // 校验验证码
+    checkCode(code) {
+      return checkAuthCode(code)
+    },
+    // 获取验证码
+    getPhoneCode() {
+      // TODO:获取验证码成功后，改变按钮文字
+      getCode(this.form.phone)
+    },
+    // 选择职位
+    selectPosition() {
+      this.showPop = true
+    },
+    onConfirm(value, index) {
+      this.form.position = value
+      this.showPop = false
+    },
+    onCancel() {
+      this.showPop = false
+    },
+    submit() {
+      console.log('提交')
+    },
   },
 }
 </script>
@@ -239,7 +319,7 @@ export default {
         font-family: PingFang SC;
         font-weight: bold;
         color: #1a1a1a;
-        margin-top: 32px;
+        margin-top: 20px;
       }
       &-desc {
         font-size: 24px;
@@ -254,6 +334,7 @@ export default {
   /deep/.sp-bottombar {
     height: 96px;
     padding: 32px 40px;
+    z-index: 999;
     &-button {
       height: 96px;
       font-family: PingFang SC;
