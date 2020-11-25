c9168731e5d206cf607474604aa4a88ab5e18c40
zky <zhengkaiyuan@dgg.net>
2020-11-25 19:59:48 +0800

    feat:产品分类

--- client/pages/category/index.vue
+++ client/pages/category/index.vue
@@ -16,48 +16,47 @@
     <!--E 头部-->
     <!--S 内容区-->
     <div class="category_con">
-      <!--S 左侧导航栏-->
-      <div class="category_con_lf">
-        <div ref="l_list" class="category_con_lf_container">
-          <ul>
-            <li
-              v-for="(item, index) in navList"
-              :key="index"
-              ref="l_item"
-              class="category_con_lf_container_item"
-              @click="handleClick(index)"
-            >
-              {{ item.title }}
-            </li>
-          </ul>
-        </div>
-      </div>
-      <!--E 左侧导航栏-->
-      <!--S 右侧内容区-->
-      <div ref="rightMenu" class="category_con_rt">
-        <div ref="r_list" class="category_con_rt_container">
-          <ul>
-            <li
-              v-for="(item, index) in navList"
-              ref="r_item"
-              :key="index"
-              class="right_item proList"
-            >
-              <p class="title">{{ item.title }}</p>
-              <div class="item_con">
-                <div
-                  v-for="(cItem, cIndex) in item.children"
-                  :key="cIndex"
-                  class="child"
-                >
-                  {{ cItem.title }}
-                </div>
+      <!--S 侧边栏区域-->
+      <aside ref="l_list" class="category_con_lf">
+        <ul>
+          <li
+            v-for="(item, index) in navList"
+            ref="l_item"
+            :key="index"
+            class="category_con_lf_item"
+            :style="{
+              backgroundColor: TabNavList == index ? '#fff' : '#f8f8f8',
+            }"
+            @click="handleClick(index)"
+          >
+            {{ item.title }}
+          </li>
+        </ul>
+      </aside>
+      <!--S 侧边栏区域-->
+      <!--S 二级分类区域-->
+      <section ref="r_list" class="category_con_rt">
+        <div>
+          <div
+            v-for="(item, index) in navList"
+            :key="index"
+            ref="good"
+            class="proList"
+          >
+            <div class="title">{{ item.title }}</div>
+            <div class="item_con">
+              <div
+                v-for="(cItem, cIndex) in item.children"
+                :key="cIndex"
+                class="item_con_child"
+              >
+                {{ cItem.title }}
               </div>
-            </li>
-          </ul>
+            </div>
+          </div>
         </div>
-      </div>
-      <!--S 右侧内容区-->
+      </section>
+      <!--E 二级分类区域-->
     </div>
     <!--E 内容区-->
   </div>
@@ -191,12 +190,71 @@ export default {
             { title: '二级导航-2-3' },
           ],
         },
+        {
+          title: '一级导航-15',
+          children: [
+            { title: '二级导航-2-1' },
+            { title: '二级导航-2-2' },
+            { title: '二级导航-2-3' },
+          ],
+        },
+        {
+          title: '一级导航-15',
+          children: [
+            { title: '二级导航-2-1' },
+            { title: '二级导航-2-2' },
+            { title: '二级导航-2-3' },
+          ],
+        },
+        {
+          title: '一级导航-15',
+          children: [
+            { title: '二级导航-2-1' },
+            { title: '二级导航-2-2' },
+            { title: '二级导航-2-3' },
+          ],
+        },
+        {
+          title: '一级导航-15',
+          children: [
+            { title: '二级导航-2-1' },
+            { title: '二级导航-2-2' },
+            { title: '二级导航-2-3' },
+          ],
+        },
+        {
+          title: '一级导航-15',
+          children: [
+            { title: '二级导航-2-1' },
+            { title: '二级导航-2-2' },
+            { title: '二级导航-2-3' },
+          ],
+        },
+        {
+          title: '一级导航-15',
+          children: [
+            { title: '二级导航-2-1' },
+            { title: '二级导航-2-2' },
+            { title: '二级导航-2-3' },
+          ],
+        },
+        {
+          title: '一级导航-15',
+          children: [
+            { title: '二级导航-2-1' },
+            { title: '二级导航-2-2' },
+            { title: '二级导航-2-3' },
+          ],
+        },
       ], // 全局列表
       req: [
         'https://img.yzcdn.cn/vant/cat.jpeg',
         'https://img.yzcdn.cn/vant/cat.jpeg',
       ],
-      heightList: [], // 高度集合
+      arr: [0], // 高度集合
+      scrollY: 0,
+      TabNavList: 0, // 左右联动取值
+      flag: true,
     }
   },
   mounted() {
@@ -207,28 +265,60 @@ export default {
   },
   methods: {
     inputChange() {},
-    _getHeight() {
-      // 获取内容区中的每一块的高度
-      const rItems = this.$refs.r_item
-      const hList = []
-      rItems.forEach((item) => {
-        hList.push(item.clientHeight)
-      })
-      // 将右侧内容区所有模块的高度集合在数组中
-      this.heightList = hList
-    },
     _initScroll() {
+      // 初始化滚动事件
       this.left = new Better(this.$refs.l_list, {
+        click: true,
+        probeType: 3,
+      })
+      this.rgt = new Better(this.$refs.r_list, {
         probeType: 3,
         click: true,
       })
-      this.right = new Better(this.$refs.r_list, {
-        probeType: 3,
-        click: true,
+      this.rgt.on('scroll', (res) => {
+        if (this.flag) {
+          this.scrollY = Math.abs(res.y) + 64
+          console.log('scrollY', this.scrollY)
+          for (let i = 0; i < this.arr.length; i++) {
+            if (this.scrollY > this.arr[i] && this.scrollY < this.arr[i + 1]) {
+              this.TabNavList = i - 1 // 左右联动取值
+              // document.getElementById(this.TabNavList).scrollIntoView()
+              this.left.scrollToElement(
+                this.$refs.l_list,
+                100,
+                0,
+                this.TabNavList * 62
+              )
+            } else {
+              console.log(222222)
+            }
+          }
+        }
       })
+    },
+    _getHeight() {
+      // 集合右侧内容模块高度
+      const rightItems = this.$refs.r_list.getElementsByClassName('proList')
+      setTimeout(() => {
+        // 根据betterScroll定义滚动
+        if (rightItems && rightItems.length > 0) {
+          let height = 0
+          this.arr.push(height)
+          for (let i = 0; i < rightItems.length; i++) {
+            const item = rightItems[i]
+            height += item.clientHeight
+            this.arr.push(height)
+          }
+        }
+      }, 600)
     },
     handleClick(index) {
-      this.right.scrollTo(0, -this.heightList[index], 400)
+      this.flag = false
+      this.TabNavList = index // 左右联动取值
+      this.rgt.scrollToElement(this.$refs.good[index], 100, 0, 0)
+      setTimeout(() => {
+        this.flag = true
+      }, 100)
     },
   },
 }
@@ -282,65 +372,72 @@ export default {
     }
   }
   &_con {
-    margin: 128px 0 0 0;
+    margin: 0 auto;
     width: 100%;
     height: 100%;
+    padding-top: 128px;
     overflow: hidden;
     position: relative;
     display: flex;
     &_lf {
+      display: block;
       width: 200px;
-      flex: 0 0 200px;
-      background-color: #f8f8f8;
+      background: #f8f8f8;
+      overflow: hidden;
+      position: relative;
       overflow-y: scroll;
-      &_container {
-        &_item {
-          width: 100%;
-          height: 124px;
-          background: #f8f8f8;
-          border-radius: 0 8px 0 0;
-          font-size: 26px;
-          font-family: PingFang SC;
-          font-weight: 400;
-          color: #555555;
-          line-height: 124px;
-          text-align: center;
-        }
+      overflow-x: hidden;
+      -webkit-overflow-scrolling: touch;
+      &::-webkit-scrollbar {
+        display: none;
+      }
+      &_item {
+        width: 200px;
+        height: 124px;
+        font-size: 26px;
+        font-family: PingFang SC;
+        font-weight: 400;
+        color: #555555;
+        text-align: center;
+        line-height: 124px;
       }
     }
     &_rt {
-      flex: 1;
+      display: block;
+      overflow: hidden;
+      background: #fff;
+      position: relative;
+      width: calc(100vw - 200px);
       padding: 0 32px;
       overflow-y: scroll;
-      &_container {
-        .right_item {
-          margin-top: 49px;
-          .title {
-            font-size: 30px;
+      overflow-x: hidden;
+      -webkit-overflow-scrolling: touch;
+      .proList {
+        padding-top: 48px;
+        .title {
+          font-size: 30px;
+          font-family: PingFang SC;
+          font-weight: bold;
+          color: #222222;
+        }
+        .item_con {
+          display: flex;
+          justify-content: flex-start;
+          align-items: center;
+          flex-direction: row;
+          flex-wrap: wrap;
+          &_child {
+            height: 60px;
+            background: #ffffff;
+            border: 1px solid #cdcdcd;
+            border-radius: 4px;
+            line-height: 60px;
+            margin: 32px 32px 0 0;
+            font-size: 24px;
             font-family: PingFang SC;
-            font-weight: bold;
-            color: #222222;
-          }
-          .item_con {
-            display: flex;
-            justify-content: flex-start;
-            align-items: center;
-            flex-direction: row;
-            flex-wrap: wrap;
-            .child {
-              padding: 0 21px;
-              height: 60px;
-              background: #ffffff;
-              border: 1px solid #cdcdcd;
-              border-radius: 4px;
-              margin: 32px 32px 0 0;
-              font-size: 24px;
-              font-family: PingFang SC;
-              font-weight: 400;
-              color: #555555;
-              line-height: 60px;
-              text-align: center;
-            }
+            font-weight: 400;
+            color: #555555;
+            padding: 0 21px;
           }
         }
       }
