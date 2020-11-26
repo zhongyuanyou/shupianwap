3fd24c94d8d9fcf298a4c135bb689b911bd9d78c
z2300520z <396636246@qq.com>
2020-11-26 08:52:05 +0800

    feat:问题解答页面

---
+++ client/pages/my/help/questions.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="wrapper">
+    <Header title="问题解答" @leftClickFuc="onClickLeft" />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/common/my/header/header'
+export default {
+  name: 'Questions',
+  components: { Header },
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {
+    onClickLeft() {
+      this.$router.back(-1)
+    },
+  },
+}
+</script>
+<style lang="less" scoped>
+.wrapper {
+}
+</style>
