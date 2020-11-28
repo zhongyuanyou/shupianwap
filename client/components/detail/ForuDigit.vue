f9963faf4a97654be93b252482b16e8c9da842b9
N4K3PTUKHSZKKS6\Administrator <1076153622@qq.com>
2020-11-28 18:00:11 +0800

    feat: 400搜索页面

---
+++ client/components/detail/ForuDigit.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="foru-digit">
+    <ul class="item-list" @click="itemChange">
+      <li v-for="(elem, i) of child" :key="i">
+        {{ elem }}
+      </li>
+    </ul>
+    <div class="button">
+      <BottomConfirm
+        @resetFilters="resetFilters"
+        @confirmFilters="confirmFilters"
+      />
+    </div>
+  </div>
+</template>
+<script>
+import {
+  Toast,
+  Icon,
+  WorkTabFilter,
+  WorkTabFilterItem,
+} from '@chipspc/vant-dgg'
+import BottomConfirm from '../common/filters/BottomConfirm'
+export default {
+  name: 'BeforFoure',
+  components: {
+    BottomConfirm,
+  },
+  props: {
+    child: {
+      type: Array,
+      required: true,
+    },
+  },
+  data() {
+    return {}
+  },
+  methods: {
+    itemChange(e) {
+      if (e.target.nodeName === 'LI' && e.target.dataset.n === '1') {
+        console.log(111)
+      } else {
+        e.target.dataset.n = '1'
+        console.log(222)
+      }
+    },
+    resetFilters() {
+      alert('重置')
+    },
+    confirmFilters() {
+      alert('确定')
+    },
+  },
+}
+</script>
+<style lang="less" scoped>
+.foru-digit {
+  font-size: 24px;
+  color: #222222;
+  .item-list {
+    display: flex;
+    flex-wrap: wrap;
+    padding-left: 26px;
+    li {
+      width: 250px;
+      background: #f0f4ff;
+      width: 149px;
+      height: 64px;
+      line-height: 64px;
+      margin: 24px 12.5px;
+      text-align: center;
+    }
+  }
+}
+</style>
---
+++ client/pages/detail/selectPhone.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="select-phone">
+    <div class="top">
+      <sp-top-nav-bar
+        padding
+        @on-click-left="onClickLeft"
+        @on-click-right="onClickRight"
+      >
+        <template #left>
+          <sp-icon class-prefix="sp-iconfont" name="specialreturn2" />
+        </template>
+        <template #title>
+          <sp-nav-search
+            v-model="searchValue"
+            border
+            placeholder="请输入您喜欢的号码"
+            class="search"
+          >
+          </sp-nav-search>
+        </template>
+        <template #right> 搜索 </template>
+      </sp-top-nav-bar>
+    </div>
+    <div class="item">
+      <sp-dropdown-menu>
+        <sp-dropdown-item
+          v-model="value1"
+          title-class=""
+          :title="dropdownTitle1"
+        >
+          <ForuDigit :child="befor_foru" />
+        </sp-dropdown-item>
+        <sp-dropdown-item
+          v-model="value1"
+          title-class=""
+          :title="dropdownTitle2"
+          ><ForuDigit :child="befor_foru2" />
+        </sp-dropdown-item>
+        <sp-dropdown-item
+          v-model="value1"
+          title-class=""
+          :title="dropdownTitle3"
+        >
+          <PriceFilter
+            :price-list="priceList"
+            @minInput="minInput"
+            @maxInput="maxInput"
+          />
+        </sp-dropdown-item>
+        <sp-dropdown-item v-model="sortValue" :options="option2" />
+      </sp-dropdown-menu>
+    </div>
+  </div>
+</template>
+<script>
+import {
+  TopNavBar,
+  NavSearch,
+  Toast,
+  Icon,
+  DropdownMenu,
+  DropdownItem,
+} from '@chipspc/vant-dgg'
+import ForuDigit from '@/components/detail/ForuDigit'
+import PriceFilter from '@/components/common/filters/PriceFilter'
+export default {
+  name: 'SelectPhone',
+  components: {
+    [TopNavBar.name]: TopNavBar,
+    [NavSearch.name]: NavSearch,
+    [Toast.name]: Toast,
+    [Icon.name]: Icon,
+    [DropdownItem.name]: DropdownItem,
+    [DropdownMenu.name]: DropdownMenu,
+    ForuDigit,
+    PriceFilter,
+  },
+  data() {
+    return {
+      // show: true,
+      searchValue: null,
+      sortValue: 'a',
+      activeNames: ['1'],
+      dropdownTitle1: '前四位',
+      dropdownTitle2: '后四位',
+      dropdownTitle3: '价格',
+      value1: 0,
+      option2: [
+        { text: '默认排序', value: 'a' },
+        { text: '好评排序', value: 'b' },
+        { text: '销量排序', value: 'c' },
+      ],
+      befor_foru: ['全部', 'AAA*', 'AAB', 'AAB', 'AAC', 'AAD'],
+      befor_foru2: ['全部', 'AAA*', 'AAB', 'AAB', 'AAC', 'AAD'],
+    }
+  },
+  methods: {
+    showPopup() {
+      this.show = true
+    },
+    onClickRight() {
+      // 左侧按钮事件
+      Toast('按钮')
+    },
+    onClickLeft() {
+      // 右侧按钮
+      Toast('返回')
+    },
+  },
+}
+</script>
+<style lang="less" scoped>
+.selectPhone {
+  .top {
+    /deep/.search {
+      width: 100%;
+    }
+  }
+}
+</style>
