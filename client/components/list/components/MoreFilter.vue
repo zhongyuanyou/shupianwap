19d676084143864131c0ec6fe8a9b3eda9e29c65
薛轶戈 <xueyige@dgg.net>
2020-12-01 16:48:12 +0800

    feat:  交易列表筛选更多筛选项

--- client/components/list/components/MoreFilter.vue
+++ client/components/list/components/MoreFilter.vue
@@ -12,31 +12,43 @@
         maxHeight: maxHeight,
       }"
     >
-      <div class="more-item">
+      <div v-for="(item, index) in children" :key="index" class="more-item">
         <div class="more-item__title">
-          <h2>行业类型</h2>
-          <span
-            >全部<my-icon
-              name="tab_ic_all_n"
-              size="0.56rem"
-              color="rgba(0, 0, 0, 0.4)"
-            ></my-icon
-          ></span>
-          <select-check-box
-            ref="selectCheckBox"
-            :select-list="selectList"
-            :gutter="12"
-            :is-show-all="true"
-            :is-select-more="isSelectMore"
-            @cancelItem="cancelItem"
-            @selectItems="selectItems"
-            @selectAllItems="selectAllItems"
-          />
+          <h2>{{ item.title }}</h2>
+          <div v-if="isShowBtn[index]" @click="showAllList(index)">
+            全部
+            <div
+              :class="{
+                show_all: selectBoxVue[index].selfIsShowAll,
+              }"
+            >
+              <my-icon
+                name="tab_ic_all_n"
+                size="0.16rem"
+                color="#4974f5"
+              ></my-icon>
+            </div>
+          </div>
         </div>
+        <select-check-box
+          :select-list="item.filters"
+          :gutter="12"
+          :is-select-more="item.isSelects"
+          :self-active-item="activeItems[index]"
+          @isShowBtnHandle="
+            (_flag, _this) => {
+              isShowBtnHandle(_flag, index, _this)
+            }
+          "
+          @selectItems="
+            (_item, _items) => {
+              selectItems(_item, _items, index)
+            }
+          "
+        />
       </div>
     </div>
     <BottomConfirm
-      v-if="isSelectMore"
       @resetFilters="resetFilters"
       @confirmFilters="confirmFilters"
     />
@@ -44,9 +56,16 @@
 </template>
 
 <script>
+/**
+ *
+ * 回显数据
+ * 提交数据
+ * 重置筛选
+ * */
 import { DropdownItem } from '@chipspc/vant-dgg'
 import SelectCheckBox from '@/components/common/filters/SelectCheckBox'
 import BottomConfirm from '@/components/common/filters/BottomConfirm'
+import clone from '~/utils/clone'
 export default {
   name: 'MoreFilter',
   components: {
@@ -66,8 +85,227 @@ export default {
     return {
       moreTextCss: '',
       dropdownTitle: '',
-      isSelectMore: false,
-      selectList: [],
+      children: [
+        {
+          title: '行业类型',
+          filters: [
+            {
+              id: '1',
+              name: '科技信息',
+            },
+            {
+              id: '2',
+              name: '科技信息',
+            },
+            {
+              id: '3',
+              name: '科技信息',
+            },
+            {
+              id: '4',
+              name: '科技信息',
+            },
+            {
+              id: '5',
+              name: '科技信息',
+            },
+            {
+              id: '6',
+              name: '科技信息',
+            },
+            {
+              id: '7',
+              name: '科技信息',
+            },
+            {
+              id: '1',
+              name: '科技信息',
+            },
+            {
+              id: '2',
+              name: '科技信息',
+            },
+            {
+              id: '3',
+              name: '科技信息',
+            },
+            {
+              id: '4',
+              name: '科技信息',
+            },
+            {
+              id: '5',
+              name: '科技信息',
+            },
+            {
+              id: '6',
+              name: '科技信息',
+            },
+            {
+              id: '7',
+              name: '科技信息',
+            },
+            {
+              id: '1',
+              name: '科技信息',
+            },
+            {
+              id: '2',
+              name: '科技信息',
+            },
+            {
+              id: '3',
+              name: '科技信息',
+            },
+            {
+              id: '4',
+              name: '科技信息',
+            },
+            {
+              id: '5',
+              name: '科技信息',
+            },
+            {
+              id: '6',
+              name: '科技信息',
+            },
+            {
+              id: '7',
+              name: '科技信息',
+            },
+          ],
+          isSelects: false,
+        },
+        {
+          title: '行业类型2',
+          filters: [
+            {
+              id: '1',
+              name: '科技信息',
+            },
+            {
+              id: '2',
+              name: '科技信息',
+            },
+            {
+              id: '3',
+              name: '科技信息',
+            },
+            {
+              id: '4',
+              name: '科技信息',
+            },
+            {
+              id: '5',
+              name: '科技信息',
+            },
+            {
+              id: '6',
+              name: '科技信息',
+            },
+            {
+              id: '7',
+              name: '科技信息',
+            },
+            {
+              id: '1',
+              name: '科技信息',
+            },
+            {
+              id: '2',
+              name: '科技信息',
+            },
+            {
+              id: '3',
+              name: '科技信息',
+            },
+            {
+              id: '4',
+              name: '科技信息',
+            },
+            {
+              id: '5',
+              name: '科技信息',
+            },
+            {
+              id: '6',
+              name: '科技信息',
+            },
+            {
+              id: '7',
+              name: '科技信息',
+            },
+            {
+              id: '1',
+              name: '科技信息',
+            },
+            {
+              id: '2',
+              name: '科技信息',
+            },
+            {
+              id: '3',
+              name: '科技信息',
+            },
+            {
+              id: '4',
+              name: '科技信息',
+            },
+            {
+              id: '5',
+              name: '科技信息',
+            },
+            {
+              id: '6',
+              name: '科技信息',
+            },
+            {
+              id: '7',
+              name: '科技信息',
+            },
+          ],
+          isSelects: true,
+        },
+        {
+          title: '行业类型3',
+          filters: [
+            {
+              id: '1',
+              name: '科技信息',
+            },
+            {
+              id: '2',
+              name: '科技信息',
+            },
+            {
+              id: '3',
+              name: '科技信息',
+            },
+            {
+              id: '4',
+              name: '科技信息',
+            },
+            {
+              id: '5',
+              name: '科技信息',
+            },
+            {
+              id: '6',
+              name: '科技信息',
+            },
+            {
+              id: '7',
+              name: '科技信息',
+            },
+          ],
+          isSelects: false,
+        },
+      ],
+      isShowBtn: [], // 对应的筛选栏是否显示全部按钮
+      selectBoxVue: [], // 存储所有的选择栏vue实例
+      selectValueArray: [], // 所选择的数据
+      activeItems: [], // 默认激活的
+      saveActiveItems: [], // 存储的筛选项数据
     }
   },
   computed: {
@@ -82,7 +320,7 @@ export default {
     filterData(val) {
       if (val && JSON.stringify(val) !== '{}') {
         this.dropdownTitle = val.title
-        this.selectList = val.filters
+        // this.selectList = val.filters
         // this.selectList = [
         //   {
         //     id: '1',
@@ -113,35 +351,115 @@ export default {
         //     name: '科技信息',
         //   },
         // ]
-        this.isSelectMore = val.isSelects
       }
     },
   },
   mounted() {
     if (this.filterData && JSON.stringify(this.filterData) !== '{}') {
       this.dropdownTitle = this.filterData.title
-      this.selectList = this.filterData.filters
-      this.isSelectMore = this.filterData.isSelects
+      // this.selectList = this.filterData.filters
     }
   },
   methods: {
-    open() {},
-    close() {},
-    cancelItem() {},
-    selectItems(item, items) {
-      console.log(item, items)
+    isShowBtnHandle(_flag, index, _this) {
+      // 保存选择框对象实例
+      this.$set(this.isShowBtn, index, _flag)
+      // 由于不是根结点无法通过$refs去获取筛选框实例，这里通过内部抛出this来获取
+      this.$set(this.selectBoxVue, index, _this)
+      // 初始化所有的默认激活筛选栏数据
+      this.$set(this.activeItems, index, [])
+      // 初始化存储激活筛选栏数据
+      this.$set(this.saveActiveItems, index, [])
     },
-    selectAllItems() {},
+    showAllList(_index) {
+      // 显示/隐藏选择栏多余数据
+      this.selectBoxVue[_index].selfIsShowAll
+        ? this.selectBoxVue[_index].hideMore()
+        : this.selectBoxVue[_index].showAll()
+    },
+    open() {
+      this.$nextTick(() => {
+        // 重置内容据顶部的距离
+        document.querySelector('.more-content').scrollTop = 0
+      })
+    },
+    close() {
+      console.log('close')
+      // 如果关闭需要将默认筛选数据和保存的筛选数据同步，防止在选择了筛选项后没有点确定直接关闭筛选下拉框
+      this.saveActiveItems.forEach((item, index) => {
+        this.$set(this.activeItems, index, item)
+      })
+    },
+    selectItems(_item, _items, _index) {
+      // 选择详细，参数选择的item，选择的所有items，下标index
+      this.selectValueArray[_index] = _items
+      this.$set(this.activeItems, _index, _items)
+      // this.activeItems[_index] = _items
+    },
     resetFilters() {
-      this.$refs.selectCheckBox.clearSelect()
+      // 重置所有筛选选项
+      this.selectBoxVue.forEach((item) => {
+        item.clearSelect()
+      })
+      this.activeItems.forEach((item, index) => {
+        this.$set(this.activeItems, index, [])
+        // this.activeItems[index] = []
+      })
     },
-    confirmFilters() {},
+    confirmFilters() {
+      // 确认筛选
+      this.saveActiveItems = clone(this.activeItems, true)
+      this.$refs.item.toggle()
+    },
   },
 }
 </script>
 
 <style lang="less" scoped>
 .more-content {
-  padding: 56px 40px;
+  overflow-x: hidden;
+  overflow-y: scroll;
+  padding: 52px 40px 56px 40px;
+  .more-item {
+    margin-bottom: 60px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+    .more-item__title {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      margin-bottom: 28px;
+      h2 {
+        font-size: 36px;
+        font-family: PingFang SC;
+        font-weight: bold;
+        color: #222222;
+      }
+      div {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        color: #4974f5;
+        font-size: 26px;
+        /*transform-origin: center center;*/
+        /*transform: rotateX(180deg);*/
+        div {
+          display: inline;
+          .tab_ic_all_n {
+            margin-left: 12px;
+          }
+          &.show_all {
+            transform-origin: center center;
+            transition: all 0.3s;
+            transform: rotateX(180deg);
+          }
+        }
+      }
+    }
+  }
+}
+/deep/.sp-dropdown-item__content {
+  max-height: 100%;
 }
 </style>
