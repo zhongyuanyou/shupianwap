ceb8f8de8c3ccf28c4107c066b156af23b656db7
zky <zhengkaiyuan@dgg.net>
2020-12-02 13:53:45 +0800

    feat:公共打开app组件

---
+++ client/components/common/openApp/Open.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="open">
+    <div class="open_lf">
+      <sp-image
+        fit="cover"
+        width="0.57rem"
+        height="0.57rem"
+        src="https://img.yzcdn.cn/vant/cat.jpeg"
+      />
+      <span>薯片招人APP</span>
+    </div>
+    <sp-button type="primary" size="small">立即打开</sp-button>
+  </div>
+</template>
+
+<script>
+import { Image, Button } from '@chipspc/vant-dgg'
+export default {
+  name: 'Open',
+  components: {
+    [Image.name]: Image,
+    [Button.name]: Button,
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.open {
+  width: 100%;
+  padding: 0;
+  background-color: #fff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  &_lf {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    flex-direction: row;
+    span {
+      margin-left: 24px;
+      font-size: 32px;
+      font-family: PingFang SC;
+      font-weight: bold;
+      color: #1a1a1a;
+    }
+  }
+}
+</style>
--- client/components/detail/Basic.vue
+++ client/components/detail/Basic.vue
@@ -14,27 +14,20 @@
       <sp-button plain type="primary" size="small">去出价</sp-button>
     </div>
     <div class="open_app">
-      <div class="open_app_lf">
-        <sp-image
-          fit="cover"
-          width="0.57rem"
-          height="0.57rem"
-          src="https://img.yzcdn.cn/vant/cat.jpeg"
-        />
-        <span>薯片招人APP</span>
-      </div>
-      <sp-button type="primary" size="small">立即打开</sp-button>
+      <Open />
     </div>
   </div>
 </template>
 
 <script>
 import { Image, Button } from '@chipspc/vant-dgg'
+import Open from '~/components/common/openApp/Open'
 export default {
   name: 'Basic',
   components: {
     [Image.name]: Image,
     [Button.name]: Button,
+    Open,
   },
 }
 </script>
@@ -77,25 +70,7 @@ export default {
     }
   }
   .open_app {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    flex-direction: row;
     margin-top: 62px;
-    &_lf {
-      display: flex;
-      justify-content: flex-start;
-      align-items: center;
-      flex-direction: row;
-      span {
-        margin-left: 24px;
-        font-size: 32px;
-        font-family: PingFang SC;
-        font-weight: bold;
-        color: #1a1a1a;
-        line-height: 64px;
-      }
-    }
   }
 }
 </style>
