57cd1ec02548efa66772a6d6881b9e0ac75599e4
赵冬明 <zhaodongming@dgg.net>
2020-12-03 19:33:11 +0800

    feat:新增cms数据字典查询

--- config/envConfig/serveApi/index.js
+++ config/envConfig/serveApi/index.js
@@ -5,17 +5,17 @@
  * @Description: 中间层调用java服务的api地址聚合
  * @FilePath: /chips-wap/config/envConfig/serveApi/index.js
  * */
-"use strict";
-const { algorithmApi } = require("./module/algorithm"); // 算法
-const { bigDataApi } = require("./module/bigData"); // 大数据
-const { contentApi } = require("./module/content"); // CMS内容
-const { productApi } = require("./module/product"); // 产品中心
-const { userApi } = require("./module/user"); // 用户中心
+'use strict';
+const { algorithmApi } = require('./module/algorithm'); // 算法
+const { bigDataApi } = require('./module/bigData'); // 大数据
+const { contentApi } = require('./module/content'); // CMS内容
+const { productApi } = require('./module/product'); // 产品中心
+const { userApi } = require('./module/user'); // 用户中心
 
 module.exports = {
-    algorithmApi,
-    bigDataApi,
-    contentApi,
-    productApi,
-    userApi,
+  algorithmApi,
+  bigDataApi,
+  contentApi,
+  productApi,
+  userApi,
 };
