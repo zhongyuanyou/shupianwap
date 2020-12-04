6c44213d1287619b6af872c33248cc47d498cc98
赵冬明 <zhaodongming@dgg.net>
2020-12-04 12:01:06 +0800

    feat:新增数据字典单层级查询接口，切换网关地址

--- app/controller/dict.js
+++ app/controller/dict.js
@@ -1,18 +1,16 @@
 'use strict';
 const Controller = require('egg').Controller;
 const { Get, Prefix } = require('egg-shell-decorators');
+const { contentApi } = require('./../../config/serveApi/index');
 
-@Prefix('/nk/dict/v1')
+@Prefix('/nk/dict')
 class DictController extends Controller {
-  @Get('/cms_code.do')
+  @Get('/v1/cms_code.do')
   async queryCmsCode() {
     const { ctx, service, app } = this;
     // 定义参数校验规则
     const rules = {
-      categoryCode: { type: 'string', required: false },
-      keyword: { type: 'string', required: false },
-      limit: { type: 'number', required: true },
-      page: { type: 'number', required: true },
+      code: { type: 'string', required: true },
     };
     // 参数校验
     const valiErrors = app.validator.validate(rules, ctx.query);
@@ -22,15 +20,56 @@ class DictController extends Controller {
       return;
     }
     // 参数校验通过,正常响应
-    // const { pageNumber = 10, pageSize = 1, id } = ctx.query;
-    // const { status, data } = await service.curl.curlAll('/nk/dict/v1/find_list.do', {
-    //   method: 'GET',
-    //   data: {
-    //     pageNumber,
-    //     pageSize,
-    //     teamId: id,
-    //   },
-    // });
+    const { code } = ctx.query;
+    const sysCode = app.config.apiClient.APPID[0];
+    const host = ctx.helper.getUrl(sysCode);
+    const address = contentApi.dataDict;
+    const url = `${host}/${sysCode}${address}`;
+    const { status, data } = await service.curl.curlAll(url, {
+      method: 'GET',
+      data: {
+        code,
+      },
+    });
+    if (status === 200 && data.code === 200) {
+      ctx.helper.success({ ctx, code: 200, res: data.data });
+    } else {
+      ctx.logger.error(status, data);
+      ctx.helper.fail({ ctx, code: 500, res: '后端接口异常！' });
+    }
+  }
+  @Get('/v1/cms_codes.do')
+  async queryCmsCodes() {
+    const { ctx, service, app } = this;
+    // 定义参数校验规则
+    const rules = {
+      code: { type: 'array', required: true },
+    };
+    // 参数校验
+    const valiErrors = app.validator.validate(rules, ctx.query);
+    // 参数校验未通过
+    if (valiErrors) {
+      ctx.helper.fail({ ctx, code: 422, res: valiErrors });
+      return;
+    }
+    // 参数校验通过,正常响应
+    const { code } = ctx.query;
+    const sysCode = ctx.config.apiClient.APPID[0];
+    const host = ctx.helper.getUrl(sysCode);
+    const address = contentApi.dataDicts;
+    const url = `${host}/${sysCode}${address}`;
+    const { status, data } = await service.curl.curlAll(url, {
+      method: 'GET',
+      data: {
+        code,
+      },
+    });
+    if (status === 200 && data.code === 200) {
+      ctx.helper.success({ ctx, code: 200, res: data.data });
+    } else {
+      ctx.logger.error(status, data);
+      ctx.helper.fail({ ctx, code: 500, res: '后端接口异常！' });
+    }
   }
 }
 
