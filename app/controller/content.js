1f195e01c5d9bdd57547d4bb3940edb18963ae6e
薛轶戈 <xueyige@dgg.net>
2020-12-07 10:37:01 +0800

    feat:  添加内容规则

--- app/controller/content.js
+++ app/controller/content.js
@@ -2,34 +2,22 @@
 const Controller = require('egg').Controller;
 const { Get, Prefix } = require('egg-shell-decorators');
 const { contentApi } = require('./../../config/serveApi/index');
+const { getContentListValidate } = require('../validate/contentRules');
 @Prefix('/yk/content')
-class ContentListController extends Controller {
+class ContentController extends Controller {
   @Get('/v1/content_list.do')
   async getContentList () {
     const { ctx, service, app } = this;
     // 定义参数校验规则
-    const rules = {
-      categoryCode: { type: 'string', required: false },
-      categoryId: { type: 'string', required: false },
-      keyword: { type: 'string', required: false },
-      limit: { type: 'Number', required: false },
-      page: { type: 'Number', required: false },
-      platformCode: { type: 'string', required: false },
-      terminalCode: { type: 'string', required: false },
-      sortOrder: { type: 'string', required: false },
-      type: { type: 'string', required: false },
-    };
-    // 参数校验
-    const valiErrors = app.validator.validate(rules, ctx.query);
+    const valiErrors = getContentListValidate(this)
     // 参数校验未通过
     if (valiErrors) {
       ctx.helper.fail({ ctx, code: 422, res: valiErrors });
       return;
     }
     const sysCode = app.config.apiClient.APPID[0];
-    const host = ctx.helper.getUrl(sysCode);
     const address = contentApi.findPage;
-    const url = `${host}/${address}`;
+    const url = ctx.helper.assembleUrl(sysCode, address);
     ctx.query.limit = ctx.query.limit ? ctx.query.limit : 10
     ctx.query.page = ctx.query.page ? ctx.query.page : 10
     const { status, data } = await service.curl.curlAll(url, {
@@ -45,4 +33,4 @@ class ContentListController extends Controller {
   }
 }
 
-module.exports = ContentListController
+module.exports = ContentController
---
+++ app/validate/contentRules.js
@@ -0,0 +1,20 @@
+'use strict';
+module.exports = {
+  getContentListValidate(_this) {
+    const { ctx, app } = _this;
+    const rules = {
+      categoryCode: { type: 'string', required: false },
+      categoryId: { type: 'string', required: false },
+      keyword: { type: 'string', required: false },
+      limit: { type: 'Number', required: false },
+      page: { type: 'Number', required: false },
+      platformCode: { type: 'string', required: false },
+      terminalCode: { type: 'string', required: false },
+      sortOrder: { type: 'string', required: false },
+      type: { type: 'string', required: false },
+    };
+    // 参数校验
+    const valiErrors = app.validator.validate(rules, ctx.query);
+    return valiErrors
+  }
+}
