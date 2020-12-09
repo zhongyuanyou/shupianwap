0cb5080c47e881e39a2e5e65dd6e773cbb581232
薛轶戈 <xueyige@dgg.net>
2020-12-09 13:47:42 +0800

    feat： 增加获取产品分类的controller

--- app/controller/category.js
+++ app/controller/category.js
@@ -1,13 +1,14 @@
 'use strict';
 
 const Controller = require('egg').Controller;
-const { Get, Prefix } = require('egg-shell-decorators');
+const { Get, Post, Prefix } = require('egg-shell-decorators');
 const { getCategoryDetailValidate } = require('../validate/categoryRules')
 
 @Prefix('/nk/category')
 class CategoryController extends Controller {
   @Get('/v1/category_detail.do')
   async getCategory() {
+    // 获取CMS分类
     const { ctx, service } = this;
     // 进行参数校验
     const valiErrors = getCategoryDetailValidate(this)
@@ -21,6 +22,34 @@ class CategoryController extends Controller {
       ctx.helper.success({ ctx, code: 200, res: data.data });
     } else {
       ctx.logger.error(status, data);
+      ctx.helper.fail({ ctx, code: 500, res: '后端接口异常！' });
+    }
+  }
+  @Get('/v1/get_product_category.do')
+  async getJyProductCategory() {
+    // 获取产品的分类
+    const { ctx, service, app } = this;
+    try {
+      // 定义参数校验规则
+      const rules = {
+        productTypeCode: { type: 'string', required: true }, // 产品类型
+      };
+      // 参数校验
+      const valiErrors = app.validator.validate(rules, ctx.query);
+      // 参数校验未通过
+      if (valiErrors) {
+        ctx.helper.fail({ ctx, code: 422, res: valiErrors });
+        return;
+      }
+      const {status, data} = await service.common.category.getProductCategory(ctx.query.productTypeCode);
+      // 成功之后的响应
+      if (status === 200 && data.code === 200) {
+        ctx.helper.success({ ctx, code: 200, res: data });
+      } else {
+        ctx.helper.fail({ ctx, code: 500, res: data.message });
+      }
+    } catch (e) {
+      ctx.logger.error(e);
       ctx.helper.fail({ ctx, code: 500, res: '后端接口异常！' });
     }
   }
