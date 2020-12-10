db121557935ea516687721d75d9861633deee364
maliang <maliang1@dgg.net>
2020-12-10 11:27:40 +0800

    feat: 首页接口联调

--- client/api/index.js
+++ client/api/index.js
@@ -1,9 +1,9 @@
 import demo from './demo'
 import auth from './auth'
 import category from './category'
+import { homeApi } from './home' // 首页相关接口
 
-export { auth }
-export { category }
+export { auth, category, homeApi }
 export default {
   demo,
   auth,
