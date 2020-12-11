08ce837c5b7597ce7946ca373ef3f6d20f182e1b
zky <zhengkaiyuan@dgg.net>
2020-12-11 11:50:36 +0800

    feat:精度计算

--- client/api/index.js
+++ client/api/index.js
@@ -3,8 +3,10 @@ import category from './category'
 import userInfo from './userInfo'
 import { homeApi } from './home' // 首页相关接口
 import goods from './goods'
+import { foundApi } from './found'
 export { auth }
 export { category }
 export { userInfo }
 export { goods }
 export { homeApi }
+export { foundApi }
