de11176ce7b9567bb6c1594ea8ca85494f8289ae
方泓皓 <fanghonghao@dgg.net>
2020-12-14 20:33:31 +0800

    tax init

---
+++ client/components/spread/tax/banner.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <sp-nav-bar title="税务筹划">
+      <template #left>
+        <sp-icon class-prefix="sp-iconfont" name="specialreturn2" />
+      </template>
+    </sp-nav-bar>
+  </div>
+</template>
+
+<script>
+import { Icon, NavBar } from '@chipspc/vant-dgg'
+export default {
+  name: 'HeadVue',
+  components: {
+    [NavBar.name]: NavBar,
+    [Icon.name]: Icon,
+  },
+  methods: {},
+}
+</script>
+
+<style scoped></style>
---
+++ client/pages/spread/tax/tax.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Head :title="title"></Head>
+    <Banner></Banner>
+  </div>
+</template>
+
+<script>
+import Head from '@/components/common/head/header'
+import Banner from '@/components/spread/tax/banner'
+export default {
+  name: 'TaxVue',
+  components: {
+    Head,
+    Banner,
+  },
+  data() {
+    return {
+      title: '税务筹划',
+    }
+  },
+}
+</script>
+
+<style scoped></style>
