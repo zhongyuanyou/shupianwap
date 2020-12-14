5877ade7e8f85020decdccaac6df052b4860b0ea
ying.xu <273193403@qq.com>
2020-12-14 13:47:50 +0800

    feat：新建推广页文件夹

---
+++ client/pages/spread/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="page-content">
+    <Header title="工商财税" />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/common/head/header'
+
+export default {
+  name: 'Index',
+  components: {
+    Header,
+  },
+}
+</script>
+
+<style scoped></style>
