c0c3a830b910058e9360bde4908193e40c77d375
薛轶戈 <xueyige@dgg.net>
2020-12-14 11:35:55 +0800

    feat：  增加字典查询接口

--- client/api/index.js
+++ client/api/index.js
@@ -2,8 +2,9 @@ import auth from './auth'
 import shoppingCar from './shoppingCar'
 import category from './category'
 import userInfo from './userInfo'
+import dict from './dict' // 字典查询相关接口
 import { homeApi } from './home' // 首页相关接口
-import goods from './goods'
+import goods from './goods' // 商品相关查询接口
 import { foundApi } from './found'
 import { publicApi } from './public'
 import productDetailsApi from './productDetails'
@@ -15,3 +16,4 @@ export { homeApi }
 export { foundApi }
 export { publicApi }
 export { productDetailsApi }
+export { dict }
