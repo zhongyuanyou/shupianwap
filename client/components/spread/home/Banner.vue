9909a1386c2779b9cfeb4d51b59111fc7f18968a
ying.xu <273193403@qq.com>
2020-12-14 14:25:41 +0800

    fixed：新建assets目录下spreadImages文件夹，放置推广页图片

---
+++ client/components/spread/home/Banner.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <sp-swipe style="height: 120px" vertical>
+      <sp-swipe-item v-for="(item, index) in images" :key="index">
+        <sp-image fit="cover" :src="item" />
+      </sp-swipe-item>
+    </sp-swipe>
+  </div>
+</template>
+
+<script>
+import { Swipe, SwipeItem, Image } from '@chipspc/vant-dgg'
+
+export default {
+  name: 'Banner',
+  components: {
+    [Swipe.name]: Swipe,
+    [SwipeItem.name]: SwipeItem,
+    [Image.name]: Image,
+  },
+  props: {
+    images: {
+      type: Array,
+      default: () => {
+        return []
+      },
+    },
+  },
+  data() {
+    return {
+      current: 0, // banner下标
+    }
+  },
+}
+</script>
+
+<style scoped></style>
--- client/pages/spread/index.vue
+++ client/pages/spread/index.vue
@@ -1,16 +1,30 @@
 <template>
   <div class="page-content">
     <Header title="工商财税" />
+    <!--    START 轮播图-->
+    <Banner :images="images" />
+    <!--    END 轮播图-->
   </div>
 </template>
 
 <script>
 import Header from '@/components/common/head/header'
+import Banner from '@/components/spread/home/Banner'
 
 export default {
   name: 'Index',
   components: {
+    Banner,
     Header,
+  },
+  data() {
+    return {
+      images: [
+        'https://img.yzcdn.cn/vant/apple-1.jpg',
+        'https://img.yzcdn.cn/vant/apple-2.jpg',
+        'https://img.yzcdn.cn/vant/cat.jpeg',
+      ],
+    }
   },
 }
 </script>
