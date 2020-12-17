2dbe9dfd84c92861c881b3147a31e7cb113cf682
薛轶戈 <xueyige@dgg.net>
2020-12-17 16:03:06 +0800

    fix： 删除无用代码

--- app/service/common/category.js
+++ app/service/common/category.js
@@ -22,7 +22,6 @@ class categoryService extends Service {
       }
       try {
         // 忽略fegin校验条件
-        ctx.headers['X-User-Agent'] = '4b43c3f3-d817-4576-95b1-ad8519a2f14e';
         const result = await service.curl.curlGet(url, { code, id });
         resolve(result);
       } catch (err) {
