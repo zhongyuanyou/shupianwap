940773cf18676939619ef890203da8b859869364
赵冬明 <zhaodongming@dgg.net>
2020-12-19 17:59:08 +0800

    feat:解决合并冲突

--- client/api/index.js
+++ client/api/index.js
@@ -13,7 +13,8 @@ import productDetailsApi from './productDetails'
 import complain from './complain'
 import { interviewApi } from './interview'
 import { helpApi } from './help' // 帮助中心相关接口
-import { searchApi } from './search' // 搜索相关接口import { recommendApi } from './recommend' //推荐相关接口
+import { searchApi } from './search' // 搜索相关接口
+import { recommendApi } from './recommend' // 推荐相关接口
 export { auth, shoppingCar, planner }
 export { category }
 export { userinfoApi }
@@ -28,3 +29,4 @@ export { complain }
 export { interviewApi }
 export { helpApi }
 export { searchApi }
+export { recommendApi }
