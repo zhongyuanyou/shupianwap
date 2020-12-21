5a6b33cf001c747dccb756dd699db10a243b46c4
方泓皓 <fanghonghao@dgg.net>
2020-12-21 16:56:26 +0800

    feat:公共组件样式调整

--- client/components/spread/tax/banner.vue
+++ client/components/spread/tax/banner.vue
@@ -54,6 +54,7 @@
         <div class="banner-bottom-form-div">
           <span>手机号</span>
           <input
+            v-model="tel"
             type="text"
             class="banner-bottom-form-input"
             placeholder="信息保护中，仅官方可见"
@@ -63,6 +64,7 @@
         <div v-if="isshow" class="banner-bottom-form-div">
           <span>验证码</span>
           <input
+            v-model="code"
             type="text"
             class="banner-bottom-form-input banner-bottom-form-inputspe"
             placeholder="请输入验证码"
@@ -70,11 +72,13 @@
           <a
             href="javascript:;"
             class="banner-bottom-form-div-a"
-            @click="send"
+            @click="testMsg"
             >{{ test }}</a
           >
         </div>
-        <button class="banner-bottom-form-button">咨询获取节税方案</button>
+        <button class="banner-bottom-form-button" @click="consultForm">
+          咨询获取节税方案
+        </button>
         <div class="banner-bottom-form-lastdiv">
           今日已提供方案：<span>{{ count }}</span
           >份
@@ -106,21 +110,16 @@ export default {
       bigfont: { big: true },
       smallfont: { small: true },
       actions: [
-        { name: 'ISO45001认证', color: '#5a79e8' },
-        { name: 'ISO14001认证' },
-        { name: 'ISO9001认证' },
-        { name: 'ISO50430认证' },
-        { name: 'ISO27001认证' },
-        { name: 'ISO13485认证' },
-        { name: '认证年审' },
-        { name: '三体系认证' },
-        { name: '双体系认证' },
-        { name: '其他认证' },
+        { name: '代理类', color: '#5a79e8' },
+        { name: '咨询类' },
+        { name: '审计类' },
       ],
       selectname: 'ISO45001认证',
       count: 126,
       test: '获取验证码',
       select: '选择税务类型',
+      tel: '',
+      code: '',
     }
   },
 
@@ -128,27 +127,54 @@ export default {
     showPopup() {
       this.show = true
     },
+    // 表头切换
     change(i) {
       this.active = i
     },
+    // 电话号码输入框聚焦显示获取验证码
     focus() {
       this.isshow = true
     },
-    send() {
-      if (this.test === '获取验证码') {
-        let i = 59
-        this.test = i + 's'
-        const time = setInterval(() => {
-          if (i > 1) {
-            i--
+    // 验证 验证码
+    testMsg() {
+      const _tel = this.tel
+      const _reg = /^1[3,4,5,6,7,8,9]\d{9}$/
+      if (_tel === '') {
+        return console.log('请输入手机号码')
+      }
+      if (!_reg.test(_tel)) {
+        return console.log('请输入正确的手机号码')
+      }
+      const Data = {
+        tel: _tel,
+        type: 'gs',
+      }
+      this.send(Data)
+    },
+    // 验证后 发送验证码
+    send(data) {
+      if (this.test === '获取验证码' || this.test === '重新发送') {
+        window.promotion.privat.getSmsCode(data, (res) => {
+          if (res.error === 0) {
+            let i = 59
             this.test = i + 's'
-          } else {
-            this.test = '获取验证码'
-            clearInterval(time)
+            const time = setInterval(() => {
+              if (i > 1) {
+                i--
+                this.test = i + 's'
+              } else {
+                this.test = '重新发送'
+                clearInterval(time)
+              }
+            }, 1000)
+            console.log(res.msg)
+            return false
           }
-        }, 1000)
+          console.log(res.msg)
+        })
       }
     },
+    // 内容选择
     onSelect(e) {
       this.selectname = e.name
       this.select = e.name
@@ -159,6 +185,73 @@ export default {
           item.color = '#232124'
         }
       }
+    },
+    // 获取当前时间作为 后台判断唯一标识
+    getDate() {
+      const timeStamp = new Date()
+      // 获取当前年
+      const currentYear = JSON.stringify(timeStamp.getFullYear())
+      // 获取当前月
+      const currentMonth = JSON.stringify(timeStamp.getMonth() + 1)
+      // 获取当前日
+      const currentDate = JSON.stringify(timeStamp.getDate())
+      const currentHour = JSON.stringify(timeStamp.getHours()) // 获取当前小时数(0-23)
+      const currentMin = JSON.stringify(timeStamp.getMinutes()) // 获取当前分钟数(0-59)
+      const currentSeconds = JSON.stringify(timeStamp.getSeconds())
+      // 获取当前时间
+      const nowTimeString =
+        currentYear +
+        currentMonth +
+        currentDate +
+        currentHour +
+        currentMin +
+        currentSeconds
+      return nowTimeString
+    },
+    consultForm() {
+      console.log(window.promotion)
+      const _tel = this.tel
+      const _code = this.code
+      const _telReg = /^1[3,4,5,6,7,8,9]\d{9}$/
+      // eslint-disable-next-line camelcase
+      const web_url = window.location.href
+      const formId = this.getDate() + _tel // 生成表单唯一识别ID，后端用于判断二级表单与一级表单关联性（当前时间+手机号码）
+      if (!_tel) {
+        console.log('请输入电话号码')
+        return
+      }
+      if (!_telReg.test(_tel)) {
+        console.log('请输入正确的电话号码')
+        return
+      }
+      if (!_code) {
+        console.log('请输入验证码')
+        return
+      }
+      const contentStr = {
+        gsmc: this.select,
+      }
+      const params = {
+        formId, // formId,唯一ID提交资源中心
+        name: '匿名客户',
+        tel: _tel, // 电话
+        url: web_url, // 链接
+        type: 'swch', // 业态编码
+        place: 'cd',
+        device: 'wap', // 设备：pc,wap
+        web: 'xmt', // 归属渠道：xmt,zytg,wxgzh
+        smsCode: _code, // 验证码
+        content: JSON.stringify(contentStr),
+      }
+      window.promotion.privat.consultForm(params, (res) => {
+        if (res.error === 0) {
+          // 这里写表单提交成功后的函数，如二级表单弹出，提示提交成功，清空DOM中表单的数据等
+          console.log(res)
+          console.log('提交成功，请注意接听电话')
+        } else {
+          console.log(res)
+        }
+      })
     },
   },
 }
