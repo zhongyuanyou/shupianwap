63a94a08091cfa37e501ffbbbd2eece3a6b67f7b
chenbin <1076153622@qq.com>
2020-12-29 20:10:09 +0800

    feat：修改了工商注册样式和输入框逻辑

--- config/envConfig/config.development.js
+++ config/envConfig/config.development.js
@@ -127,7 +127,7 @@ module.exports = (appInfo) => {
         name: 'MyOwn',
       },
       metadata: {
-        version: '3.0',
+        version: '3.2',
       },
     },
     requestMiddleware: (requestOpts, done) => {
