a3ede11a815c8c2fb73acd8187934cb5b3ec20f6
赵冬明 <zhaodongming@dgg.net>
2020-12-29 15:50:59 +0800

    fix:修改开发环境和预发布环境网关地址

--- client/config/config.release.js
+++ client/config/config.release.js
@@ -1,7 +1,7 @@
 /* 预发布配置文件 */
 
 module.exports = {
-  baseURL: 'https://dchipswg.dgg.net', // 小顶云接口
+  baseURL: 'https://dspmicrouag.shupian.cn',
   mchMerchantSgin: '63E4A4CB44669BAA672EB2DE2528CCEA',
   mchCoding: '20190620000111',
 }
