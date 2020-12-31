daf7bb6cf93c0baf527c88d793cbdf377901e513
chenbin <1076153622@qq.com>
2020-12-31 18:21:38 +0800

    feat: 工商变更

---
+++ client/components/spread/companyAlteration/TopLocation.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="company-register">
+    <div class="company-top">
+      <sp-top-nav-bar
+        background="#4653E0"
+        ellipsis
+        @on-click-left="onClickLeft"
+      >
+        <div slot="left" class="nav-back">
+          <my-icon name="nav_ic_back" size="0.40rem" color="#ffffff"></my-icon>
+        </div>
+      </sp-top-nav-bar>
+      <span class="location" @click="onMore"
+        >{{ cityName }}
+        <my-icon name="tap_ic_pen_n" size="0.08rem" color="#ffffff"></my-icon>
+      </span>
+    </div>
+  </div>
+</template>
+<script>
+import {
+  TopNavBar,
+  Icon,
+  Toast,
+  Swipe,
+  SwipeItem,
+  Lazyload,
+  Image,
+} from '@chipspc/vant-dgg'
+export default {
+  components: {
+    [TopNavBar.name]: TopNavBar,
+    [Icon.name]: Icon,
+    // 待删除--
+    [Toast.name]: Toast,
+    [Swipe.name]: Swipe,
+    [SwipeItem.name]: SwipeItem,
+    [Lazyload.name]: Lazyload,
+    [Image.name]: Image,
+  },
+  data() {
+    return {
+      cityName: '成都',
+    }
+  },
+  methods: {
+    onClickLeft() {
+      this.$router.go(-1)
+    },
+    onMore() {
+      console.log(123)
+    },
+  },
+}
+</script>
+<style lang="less" scoped>
+.company-register {
+  /deep/.company-top {
+    height: 378px;
+    background-color: #4653e0;
+    position: relative;
+    .sp-hairline--bottom::after {
+      border-bottom-width: 0;
+    }
+    .nav-back {
+      font-weight: 400;
+    }
+    .location {
+      position: absolute;
+      left: 48px;
+      bottom: 58px;
+      width: 113px;
+      height: 44px;
+      line-height: 44px;
+      background: rgba(255, 255, 255, 0.2);
+      border-radius: 22px;
+      font-size: 24px;
+      font-family: PingFang SC;
+      font-weight: bold;
+      display: block;
+      color: #ffffff;
+      text-align: center;
+      /deep/.spiconfont-tap_ic_pen_n {
+        font-weight: 400;
+        display: inline-block;
+        transform:translate(0,-3px)
+        // transform: scale(0.69);
+      }
+    }
+  }
+}
+</style>
---
+++ client/pages/spread/companyAlteration/index.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="company-register">
+    <TopLocation />
+    <div class="company-select">
+      <!-- S您需要办理哪项业务的变更服务 -->
+      <CompanySelec
+        :actions="actionsServe"
+        titleName="您需要办理哪项业务的变更服务？"
+        @onSelect="onSelectServe"
+      />
+      <!-- E您需要办理哪项业务的变更服务 -->
+      <!-- S您公司的注册在哪个区？ -->
+      <CompanySelec
+        :actions="actionsRegion"
+        titleName="您公司的注册在哪个区？"
+      />
+      <!-- S您公司的注册在哪个区？ -->
+      <SelectDesired
+        :selectList="selectActive"
+        titleName="你的身份？"
+        @onSelectActive="onDistrict"
+      />
+      <!-- E您公司的注册在哪个区？ -->
+      <!-- S办理 -->
+      <SelectDesired
+        :selectList="selectTransact"
+        titleName="您打算什么时候办理？"
+        @onSelectActive="onTransact"
+      />
+      <!-- E办理 -->
+    </div>
+  </div>
+</template>
+<script>
+import {
+  TopNavBar,
+  NavSearch,
+  Icon,
+  Toast,
+  Swipe,
+  SwipeItem,
+  Lazyload,
+  Image,
+} from '@chipspc/vant-dgg'
+import TopLocation from '@/components/spread/companyAlteration/TopLocation'
+import CompanySelec from '@/components/spread/companyAlteration/CompanySelect'
+import SelectDesired from '@/components/spread/companyAlteration/SelectDesired'
+export default {
+  components: {
+    [TopNavBar.name]: TopNavBar,
+    [NavSearch.name]: NavSearch,
+    [Icon.name]: Icon,
+    [Toast.name]: Toast,
+    [Swipe.name]: Swipe,
+    [SwipeItem.name]: SwipeItem,
+    [Lazyload.name]: Lazyload,
+    [Image.name]: Image,
+    TopLocation,
+    CompanySelec,
+    SelectDesired,
+  },
+  data() {
+    return {
+      // 选择服务
+      actionsServe: [
+        { name: '选项一*' },
+        { name: '选项二*' },
+        { name: '选项三*' },
+      ],
+      // 变更
+      actionsRegion: [
+        { name: '区域一' },
+        { name: '区域二' },
+        { name: '区域三' },
+      ],
+      // 区域
+      selectActive: [
+        {
+          name: '经办人',
+        },
+        {
+          name: '法人',
+        },
+        {
+          name: '股东',
+        },
+      ],
+      // 办理
+      selectTransact: [
+        {
+          name: '一个月内',
+        },
+        {
+          name: '两个月内',
+        },
+        {
+          name: '半年内',
+        },
+        {
+          name: '1年内',
+        },
+      ],
+    }
+  },
+  methods: {
+    onSelectServe(val) {
+      // 变更服务
+      console.log(val)
+    },
+    onDistrict(item) {
+      // 注册在哪个区
+      console.log(item)
+    },
+    onTransact(item) {
+      // 办理时间
+      console.log(item)
+    },
+  },
+}
+</script>
+<style lang="less" scoped>
+.company-register {
+  font-size: 36px;
+}
+</style>
