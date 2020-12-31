7a6dc43215524ad3b0bd765373671b3c2c5b2760
薛轶戈 <xueyige@dgg.net>
2020-12-31 09:33:16 +0800

    feat:  增加默认图片常量

---
+++ config/constant/defaultGoodsImg.js
@@ -0,0 +1,8 @@
+/** @Author: xyg
+ * @Description: 默认图片地址
+ * */
+'use strict';
+module.exports = {
+  goodsDetail: 'https://cdn.shupian.cn/sp-pt/wap/images/8n7yuuz26io0000.jpg',
+  goodsList: 'https://cdn.shupian.cn/sp-pt/wap/images/727ro8a1oa00000.jpg',
+}
