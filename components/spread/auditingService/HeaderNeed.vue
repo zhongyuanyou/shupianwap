63cce125ea735fe79aa70d9ebdcc2fc9c619f0f0
husenlin <husenlin@dgg.net>
2021-01-06 10:42:56 +0800

    style: 修改header样式

--- components/spread/auditingService/HeaderNeed.vue
+++ components/spread/auditingService/HeaderNeed.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="header">
-    <Header title="" :fixed="false" head-class="head-icon" />
     <div class="city-btn" @click="choiceCity">
       <span class="city-name">{{ currentCity }}</span>
       <my-icon name="sear_ic_open" size="0.14rem" color="#cccccc"></my-icon>
@@ -10,12 +9,8 @@
 
 <script>
 import { mapState } from 'vuex'
-import Header from '@/components/common/head/header'
 export default {
   name: 'HeaderBg',
-  components: {
-    Header,
-  },
   computed: {
     // 将接受的state混合进组件局部计算属性
     // 监听接受的state值
@@ -34,19 +29,11 @@ export default {
 <style lang="less" scoped>
 .header {
   width: 100%;
-  height: 418px;
+  height: 320px;
   margin-bottom: 64px;
   background-image: url('https://cdn.shupian.cn/sp-pt/wap/images/13zjhce6649s000.jpg');
-  background-size: 100% 418px;
+  background-size: 100% 320px;
   position: relative;
-  /deep/ .my-head {
-    background: transparent;
-    box-shadow: none;
-  }
-  /deep/ .back-icon {
-    color: #ffffff !important;
-  }
-
   .city-btn {
     padding: 0 20px;
     height: 44px;
