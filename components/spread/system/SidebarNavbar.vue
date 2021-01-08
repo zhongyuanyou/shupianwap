49160bb264dfb7637649a8a18ead456f23b96c1a
chenbin <1076153622@qq.com>
2021-01-08 19:13:25 +0800

    021231323133hhhhggg/,.g/f,fm.ffgmh,hggm,.r,m.eewm,.ewwe,mewweewf,m.cb看V领那你把吧川农看来 nm/m,.,mnlkjjlklkjlkjkjkkjlm,./l.6222222222222
    ‘’；；；；；；；；；；；‘；’；‘’‘’‘j'kll

---
+++ components/spread/system/SidebarNavbar.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="sidebar-navbar">
+    <h5>服务介绍</h5>
+    <div class="sidebar-navbar-list">
+      <sp-sidebar v-model="activeKey" @change="onChange">
+        <sp-sidebar-item
+          v-for="(item, index) of 5"
+          :key="index"
+          :title="`标签${index}`"
+        />
+      </sp-sidebar>
+    </div>
+  </div>
+</template>
+<script>
+import { Toast, Sidebar, SidebarItem } from '@chipspc/vant-dgg'
+export default {
+  name: 'Introduction',
+  components: {
+    [Toast.name]: Toast,
+    [Sidebar.name]: Sidebar,
+    [SidebarItem.name]: SidebarItem,
+  },
+  props: {
+    //     listCount: {
+    //       type: Array,
+    //       default: () => {
+    //         return []
+    //       },
+    //     },
+    //     isMore: {
+    //       type: Boolean,
+    //       default: () => {
+    //         return false
+    //       },
+    //     },
+  },
+  data() {
+    return {
+      activeKey: 0,
+    }
+  },
+  methods: {
+    onChange(index) {
+      Toast(index)
+    },
+  },
+}
+</script>
+<style lang="less" scoped>
+.sidebar-navbar {
+  h5 {
+    // 待改
+    margin: 56px 0 32px 42px;
+    height: 40px;
+    font-size: 40px;
+    font-weight: bold;
+    color: #1a1a1a;
+    line-height: 40px;
+  }
+  &-list {
+    margin: 0 40px;
+    width: 670px;
+    height: 416px;
+    background: #ffffff;
+    border: 1px solid rgba(205, 205, 205, 0.5);
+    border-radius: 4px 4px 12px 12px;
+  }
+  /deep/.sp-sidebar {
+    .sp-sidebar-item {
+      padding: 32px 0 32px 32px;
+    }
+    .sp-sidebar-item--select {
+      color: #4974f5;
+    }
+  }
+}
+</style>
---
+++ pages/spread/system/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="system">
+    <!-- S 头部分 -->
+    <Header :title="TopTitle" />
+    <!-- E 头部分 -->
+    <!-- S banner轮播 -->
+    <BannerSwipe :banner-images="bannerImages" />
+    <!-- E banner轮播 -->
+    <!-- S 表单 -->
+    <Card :card-title="cardTitle" />
+    <!-- E 表单 -->
+    <!-- S 列表 -->
+    <SystemList />
+    <!-- E 列表 -->
+    <!-- S 侧边导航 -->
+    <SidebarNavbar />
+    <!-- E 侧边导航 -->
+  </div>
+</template>
+<script>
+import Header from '@/components/common/head/header'
+import BannerSwipe from '@/components/spread/system/BannerSwiper'
+import Card from '@/components/spread/system/Card'
+import SystemList from '@/components/spread/system/ListSystem'
+import SidebarNavbar from '@/components/spread/system/SidebarNavbar'
+export default {
+  components: {
+    Header,
+    BannerSwipe,
+    Card,
+    SystemList,
+    SidebarNavbar,
+  },
+  data() {
+    return {
+      TopTitle: '体系认证',
+      bannerImages: [
+        {
+          url: '',
+          img: 'https://cdn.shupian.cn/sp-pt/wap/92ccqbb36mg0000.jpg',
+        },
+        {
+          url: '',
+          img: 'https://cdn.shupian.cn/sp-pt/wap/fzcndei5exs0000.jpg',
+        },
+      ],
+      cardTitle: '体系认证 费用测算',
+    }
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.system {
+}
+</style>
