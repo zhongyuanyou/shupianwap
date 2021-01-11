a205d41ebce88290d1ad068c65796e7abb9dcba6
薛轶戈 <xueyige@dgg.net>
2021-01-11 10:29:30 +0800

    feat： 增加cms留言接口

--- api/consult.js
+++ api/consult.js
@@ -1,12 +1,15 @@
 'use strict'
-
+/**
+ * 留言相关接库（表单提交）
+ *
+ * */
 import { CHIPS_APP_BASE_URL } from '../config/constant'
 
 import { request } from '@/utils/request'
 
 const consult = {
   getCMSCode(params) {
-    // 获取cmscode
+    // 获取cmscode（留言）
     return request({
       params,
       method: 'post',
@@ -14,7 +17,7 @@ const consult = {
     })
   },
   consultAddLogin(params) {
-    // 未登录留言
+    // 未登录留言（留言）
     return request({
       params,
       method: 'post',
@@ -22,11 +25,19 @@ const consult = {
     })
   },
   consultAdd(params) {
-    // 登录留言
+    // 登录留言（留言）
     return request({
       params,
       method: 'post',
       url: CHIPS_APP_BASE_URL + '/yk/trade/consult/consult.do',
+    })
+  },
+  cmsAddConsult(params) {
+    // cms的增加留言接口（提交到智企）
+    return request({
+      params,
+      method: 'post',
+      url: CHIPS_APP_BASE_URL + '/yk/trade/consult/cms_add_consult.do',
     })
   },
 }
