0f9b88252b2bcbd356de947856ec699979a7bed6
方泓皓 <fanghonghao@dgg.net>
2021-01-13 16:06:31 +0800

    fix:解决埋点问题

--- plugins/sp-md-jdk.js
+++ plugins/sp-md-jdk.js
@@ -1,11 +1,8 @@
 const sensors = require('sa-sdk-javascript')
-const currentDo = location.origin
+
 sensors.init({
   // 神策系统配置
-  server_url:
-    currentDo === 'https://shupian.dgg.cn'
-      ? 'https://shence.dgg.cn:6443/sa?project=production'
-      : 'https://shence.dgg.cn:6443/sa?token=default', // 请求地址
+  server_url: 'https://shence.dgg.cn:6443/sa?project=default', // 请求地址, // 数据接收地址
   is_track_single_page: true, // 单页应用页面浏览事件采集(url改变就触发)
   show_log: true, // 控制台显示数据开关
   heatmap: {
@@ -15,4 +12,5 @@ sensors.init({
   },
 })
 window.sensors = sensors
-module.exports = sensors
+sensors.quick('autoTrack')
+export default sensors
