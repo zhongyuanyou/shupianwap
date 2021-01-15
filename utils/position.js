9d51008d814c7e5ce8aaf2ee060ebfcd5d4c61cc
maliang <maliang1@dgg.net>
2021-01-15 11:09:42 +0800

    feat: 新增站点定位方法

--- utils/position.js
+++ utils/position.js
@@ -1,8 +1,25 @@
 import axios from 'axios'
+import { homeApi } from '@/api'
+
+const amapKey = '5111f49d979a509f1e96420a1b456ff4' // 高德地图key
+const regeoUrl = '/gdmap/v3/geocode/regeo' // 高德地图逆地理编码接口服务（根据经纬度获取城市信息）
+const ipUrl = '/gdmap/v3/ip' // 高德地图根据用户请求地址ip获取所在城市
+
+/**
+ * @description 获取当前定位城市 1
+ * @param {Object}
+ * @param {Object}
+ * @param {string}
+ * @param {Object}
+ * @param {Object}
+ * @param {function}
+ * @returns {Promise}
+ *
+ * request(config)
+ *
+ *
+ */
 export default () => {
-  const amapKey = '5111f49d979a509f1e96420a1b456ff4' // 高德地图key
-  const regeoUrl = '/gdmap/v3/geocode/regeo' // 高德地图逆地理编码接口服务（根据经纬度获取城市信息）
-  const ipUrl = '/gdmap/v3/ip' // 高德地图根据用户请求地址ip获取所在城市
   return new Promise((resolve, reject) => {
     const errorData = {
       type: 0,
@@ -92,6 +109,133 @@ export default () => {
           errorData.msg = '定位失败，未知异常'
       }
       reject(errorData)
+    }
+
+    // 获取用户的地理位置。使用它需要得到用户的授权
+    function agreeObtainLocation() {
+      const option = {
+        enableHighAccuracy: true,
+        timeout: 5000, // 超时时间
+        maximumAge: 0,
+      }
+      navigator.geolocation.getCurrentPosition(geoSuccess, showError, option)
+    }
+  })
+}
+
+/**
+ * @description 获取当前定位城市 2
+ * @returns {Promise}
+ * @code {Number} 定位结果code、200 定位成功； 其它失败
+ * @message {String} 定位成功或失败的描述
+ * @data {Object} 定位成功的城市code与name
+ *
+ * @remarks 使用方式： const data = await getPositonCity()
+ */
+export const getPositonCity = () => {
+  return new Promise((resolve, reject) => {
+    const returnData = {
+      code: 200, // 200 定位成功； 其它失败
+      message: '',
+      data: {},
+    }
+    // 判断浏览器是否支持地理位置接口
+    if (navigator.geolocation) {
+      // 支持
+      agreeObtainLocation()
+    } else {
+      // 不支持,定位失败
+      returnData.code = 500
+      returnData.message = '定位失败，当前浏览器不支持GPS定位'
+      resolve(returnData)
+    }
+
+    async function getCityInfo(longAndlat) {
+      try {
+        let positionCity = ''
+        // 调用高德服务，根据经纬度逆地址解析，获取城市信息
+        const cityAnalysis = await axios.get(regeoUrl, {
+          params: {
+            key: amapKey,
+            location: longAndlat,
+          },
+        })
+        if (cityAnalysis.status === 200 && cityAnalysis.data.status) {
+          positionCity = cityAnalysis.data.regeocode.addressComponent.city
+        } else {
+          returnData.code = 6
+          returnData.message = '定位失败，高德地图逆地址编码解析错误'
+          resolve(returnData)
+        }
+
+        // 定位成功，获取站点列表判断是否开通定位城市的服务
+        if (positionCity) {
+          let siteList = []
+          // 获取站点列表
+          const siteRes = await axios.get('api' + homeApi.findSiteList, {
+            headers: {
+              'x-cache-control': 'cache',
+            },
+          })
+          if (siteRes.status === 200 && siteRes.data.code === 200) {
+            siteList = siteRes.data.data.cityList
+            // 遍历城市列表，找到对应城市
+            const mateSite = siteList.find((item) => item.name === positionCity)
+            // 匹配到对应城市
+            if (mateSite) {
+              returnData.code = 200
+              returnData.message = '定位成功，匹配到对应的服务城市'
+              returnData.data = mateSite
+              resolve(returnData)
+            } else {
+              returnData.code = 7
+              returnData.message = '定位成功，但未匹配到对应的服务城市'
+              resolve(returnData)
+            }
+          } else {
+            returnData.code = 8
+            returnData.message = '定位成功，但获取站点列表失败'
+            resolve(returnData)
+          }
+        }
+      } catch (error) {
+        returnData.code = 9
+        returnData.message = error
+        resolve(returnData)
+      }
+    }
+
+    // 定位成功
+    function geoSuccess(event) {
+      const longAndlat = `${event.coords.longitude},${event.coords.latitude}` // 经纬度
+      getCityInfo(longAndlat)
+    }
+
+    // 定位失败
+    function showError(error) {
+      switch (error.code) {
+        case error.PERMISSION_DENIED:
+          returnData.code = 1
+          returnData.message =
+            '定位失败，用户拒绝请求地理定位或因非https的站点被浏览器安全策略阻止'
+          break
+        case error.POSITION_UNAVAILABLE:
+          returnData.code = 2
+          returnData.message = '定位失败，位置信息不可用'
+          break
+        case error.TIMEOUT:
+          returnData.code = 3
+          returnData.message = '定位失败，请求获取用户位置超时'
+          break
+        case error.UNKNOWN_ERROR:
+          returnData.code = 4
+          returnData.message = '定位失败，定位系统失效'
+          break
+        default:
+          returnData.code = 5
+          returnData.message = '定位失败，未知异常'
+      }
+      resolve(returnData)
     }
 
     // 获取用户的地理位置。使用它需要得到用户的授权
