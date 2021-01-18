2a87fc903d18c3d5d2030bee0afb1fb2fd6986c9
zky <zhengkaiyuan@dgg.net>
2021-01-18 15:13:16 +0800

    feat:优化发现tab下标没有对齐问题

--- pages/found/index.vue
+++ pages/found/index.vue
@@ -2,20 +2,20 @@
   <div class="found">
     <sp-top-nav-bar special-layout :placeholder="true" fixed>
       <template #title>
-        <sp-work-tabs
+        <sp-tabs
           v-model="activeTab"
           mask
           title-active-color="#222"
           @click="onClickTap"
         >
-          <sp-work-tab
+          <sp-tab
             v-for="item in information_class"
             :key="item.code"
             :title="item.name"
             :need-content="false"
           >
-          </sp-work-tab>
-        </sp-work-tabs>
+          </sp-tab>
+        </sp-tabs>
       </template>
       <template #right>
         <sp-icon name="search" size="20" @click="onClickRight" />
@@ -34,7 +34,7 @@
 </template>
 
 <script>
-import { WorkTab, WorkTabs, Icon, TopNavBar, Toast } from '@chipspc/vant-dgg'
+import { Tab, Tabs, Icon, TopNavBar, Toast } from '@chipspc/vant-dgg'
 import { mapMutations, mapState } from 'vuex'
 import Con from '~/components/found/found/Cons'
 import { foundApi } from '@/api'
@@ -44,8 +44,8 @@ export default {
   layout: 'keepAlive',
   name: 'Found',
   components: {
-    [WorkTab.name]: WorkTab,
-    [WorkTabs.name]: WorkTabs,
+    [Tab.name]: Tab,
+    [Tabs.name]: Tabs,
     [Icon.name]: Icon,
     [TopNavBar.name]: TopNavBar,
     [Toast.name]: Toast,
@@ -154,11 +154,17 @@ export default {
     padding-top: env(safe-area-inset-top);
     z-index: 5;
   }
-  /deep/ .sp-work-tab {
+  /deep/ .sp-tab {
     font-size: 32px;
+    color: #222222;
+    font-weight: bold;
   }
-  /deep/ .sp-work-tab--active {
+  /deep/ .sp-tab--active {
     font-size: 40px;
+  }
+  /deep/ .sp-tabs__line {
+    width: 32px;
+    height: 6px;
   }
   /deep/ .sp-hairline--bottom::after {
     border-bottom: none;
