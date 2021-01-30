777857a386871daa2793125be84668c7fadc24ab
薛轶戈 <xueyige@dgg.net>
2021-01-30 14:41:53 +0800

    fix： 服务列表标签只显示3条

--- components/common/app/InstallApp.vue
+++ components/common/app/InstallApp.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="install-app" v-if="false">
+  <div v-if="false" class="install-app">
     <p>保存搜索条件，重建找到心仪服务</p>
     <div class="install-app-btn" @click="openApp($event)">
       <span>安装APP</span>
