9f70a1a6e6617f4156e263638c66343cefb37899
chenguo <chenguo1@dgg.net>
2021-03-22 10:16:58 +0800

    修改隐号拨打商品详情拨打电话号码多显示//的问题

--- components/common/commodityConsultation/commodityConsultation.vue
+++ components/common/commodityConsultation/commodityConsultation.vue
@@ -181,7 +181,7 @@ export default {
         // 解密电话
         if (telData.status === 1) {
           const tel = telData.phone
-          window.location.href = `tel://${tel}`
+          window.location.href = `tel:${tel}`
         } else if (telData.status === 0) {
           Toast({
             message: '当前人员已禁用，无法拨打电话',
