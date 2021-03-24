5447fa89fe9ff84196a8232895d140ddce23101f
chenguo <chenguo1@dgg.net>
2021-03-24 10:40:18 +0800

    修改appsdk im 方法

--- plugins/app-sdk.js
+++ plugins/app-sdk.js
@@ -197,6 +197,8 @@ const appHandler = {
       name: '', // 商户用户名称
       userId: '', // 商户用户ID
       userType: '', // 用户类型
+      requireCode:'',
+      requireName:'',
     },
     fn = () => {}
   ) => {
