3cec08b9527be0852a4907e478677dcf85ec3c02
xiangbenguo <xiangbenguo@dgg.net>
2021-03-26 12:00:42 +0800

    页面编写 引入新icon

--- pages/known/home/fans.vue
+++ pages/known/home/fans.vue
@@ -1,23 +1,11 @@
 <template>
   <div class="attention_container">
     <sp-top-nav-bar
-      title="我的关注"
+      title="我的粉丝"
       :fixed="true"
       left-arrow
       @on-click-left="$router.back()"
     />
-    <sp-tabs
-      v-model="active"
-      title-active-color="#4974F5"
-      title-inactive-color="#222222"
-      @change="tabChange"
-    >
-      <sp-tab
-        v-for="(item, index) in ['用户', '规划师']"
-        :key="index"
-        :title="item"
-      ></sp-tab>
-    </sp-tabs>
 
     <sp-list
       v-model="loading"
@@ -32,21 +20,18 @@
           <div class="title">用户名</div>
           <div class="introduce">简介或签名文字简介或签名文字…</div>
         </div>
-        <div class="bt">已关注</div>
       </div>
     </sp-list>
   </div>
 </template>
 
 <script>
-import { TopNavBar, Tabs, Tab, Image, List } from '@chipspc/vant-dgg'
+import { TopNavBar, Image, List } from '@chipspc/vant-dgg'
 import { userinfoApi } from '~/api'
 export default {
   name: 'Collection',
   components: {
     [TopNavBar.name]: TopNavBar,
-    [Tabs.name]: Tabs,
-    [Tab.name]: Tab,
     [Image.name]: Image,
     [List.name]: List,
   },
@@ -95,25 +80,7 @@ export default {
     height: 88px;
   }
 
-  .sp-tabs {
-    position: fixed;
-    left: 0;
-    top: 88px;
-    z-index: 2;
-    width: 100%;
-    height: 80px;
-    border-bottom: 1px solid #dddddd;
-  }
-
-  /deep/ .sp-tabs__line {
-    width: 28px;
-    height: 6px;
-    border-radius: 3px;
-    bottom: 34px;
-  }
-
   .list_container {
-    padding-top: 80px;
     .item {
       background: #ffffff;
       padding: 28px 32px 28px;
@@ -147,18 +114,6 @@ export default {
           text-overflow: ellipsis;
           white-space: nowrap;
         }
-      }
-      .bt {
-        width: 144px;
-        height: 64px;
-        background: #f5f5f5;
-        border-radius: 8px;
-        text-align: center;
-        line-height: 64px;
-        font-size: 26px;
-        font-family: PingFangSC-Medium, PingFang SC;
-        font-weight: 500;
-        color: #999999;
       }
     }
   }
