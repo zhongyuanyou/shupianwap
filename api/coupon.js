4a01979bb60bcc2d1a8678cb3afee7f3f72ac431
李加伦 <lijialun@dgg.cn>
2021-04-03 16:30:53 +0800

    提交修改

--- api/coupon.js
+++ api/coupon.js
@@ -3,31 +3,44 @@ import { request } from '@/utils/request'
 import { CHIPS_WAP_BASE_URL } from '@/config/constant'
 
 const coupon = {
-  getCouponList({ axios }, params) {
+  getCouponList(params) {
     // 获取优惠券列表
     return request({
-      axios,
       params,
       method: 'post',
-      url: CHIPS_WAP_BASE_URL + '/nk/marketingCoupon/v2/find_my_coupon_page',
+      url: CHIPS_WAP_BASE_URL + '/nk/coupon/v2/find_my_coupon_page.do',
     })
   },
-  receiveCoupon({ axios }, params) {
+  receiveCoupon(params) {
     // 领取优惠券
     return request({
-      axios,
       params,
       method: 'post',
-      url: CHIPS_WAP_BASE_URL + 'yk/marketingCoupon/v2/receive_coupon',
+      url: CHIPS_WAP_BASE_URL + 'yk/coupon/v2/receive_coupon.do',
     })
   },
-  useWithdrawal({ axios }, params) {
+  useWithdrawal(params) {
     // 使用撤销优惠券
     return request({
-      axios,
       params,
       method: 'post',
-      url: CHIPS_WAP_BASE_URL + '/yk/marketingCoupon/v2/use_withdrawal',
+      url: CHIPS_WAP_BASE_URL + '/yk/coupon/v2/use_withdrawal.do',
+    })
+  },
+  findOrderCouponPage(params) {
+    // 查询订单可用/不可用优惠劵列表
+    return request({
+      params,
+      method: 'post',
+      url: CHIPS_WAP_BASE_URL + '/yk/coupon/v2/find_order_coupon_page.do',
+    })
+  },
+  findPage(params) {
+    // 查询优惠劵列表（领券中心、商品详情领券）
+    return request({
+      params,
+      method: 'post',
+      url: CHIPS_WAP_BASE_URL + '/yk/coupon/v2/find_page.do',
     })
   },
 }
