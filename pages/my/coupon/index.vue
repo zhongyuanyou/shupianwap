f9499fa671480b5306a1d09dc4fd0cf4dbb3d48f
李加伦 <lijialun@dgg.cn>
2021-04-21 15:45:41 +0800

    提交优惠券修改部分

--- pages/my/coupon/index.vue
+++ pages/my/coupon/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="coupon-page">
     <!-- header头部 start -->
-    <Header title="我的优惠券" />
+    <Header title="我的优惠券" @click="$router.push({ path: '/my' })" />
     <!-- header头部 end -->
     <!-- tabs切换 start -->
     <sp-tabs v-model="couponType" @click="changeTab">
@@ -12,7 +12,10 @@
     </sp-tabs>
     <!-- tabs切换 end -->
     <!-- 优惠券列表 start -->
-    <div class="coupon_list">
+    <div
+      class="coupon_list"
+      :class="responseData.length === 0 ? 'not_coupon_data' : ''"
+    >
       <div
         v-for="(item, index) in responseData"
         :key="index"
@@ -25,11 +28,11 @@
         </div>
         <div class="item-rt">
           <!-- 气泡组件 start -->
-          <Popover
+          <!-- <Popover
             :ref="'myPop' + index"
             :text="item.remark"
             @closepop="closeBox"
-          />
+          /> -->
           <!-- 气泡组件 end-->
           <!-- 右侧显示 start-->
           <div
@@ -45,13 +48,13 @@
           <div class="title" @click="goDetailPage(item)">
             {{ item.couponName }}
           </div>
-          <div ref="textpro" class="content" @click="popOver(index)">
+          <div ref="textpro" class="content">
             {{
               item.useType === 1
-                ? '全品类商品可用'
+                ? '全品类通用'
                 : item.useType === 2
-                ? '限分类商品可用'
-                : '限特定商品类可用'
+                ? '限定部分类别产品使用'
+                : '置顶产品使用'
             }}
           </div>
           <div class="date" :class="item.showColorTime ? 'expiredate' : ''">
@@ -60,7 +63,12 @@
           <!-- 右侧显示 end-->
         </div>
       </div>
+      <div v-show="responseData.length === 0" class="not_coupon">
+        <img :src="imgAddress" alt="" />
+        <span>暂无优惠券</span>
+      </div>
     </div>
+
     <!-- 优惠券列表 end -->
     <p v-if="couponType == 2" class="coupon_tip">
       当优惠券超出使用时间范围时，该优惠券即会失效。失效超过180天
@@ -72,7 +80,7 @@
 
 <script>
 import { Button, RadioGroup, Radio, Cell, Tab, Tabs } from '@chipspc/vant-dgg'
-import Popover from '~/components/common/popover/popover_old.vue'
+// import Popover from '~/components/common/popover/popover_old.vue'
 import Header from '@/components/common/head/header'
 import LoadingCenter from '@/components/common/loading/LoadingCenter'
 import { coupon } from '@/api/index'
@@ -85,11 +93,12 @@ export default {
     [Tab.name]: Tab,
     [Tabs.name]: Tabs,
     Header,
-    Popover,
+    // Popover,
     LoadingCenter,
   },
   data() {
     return {
+      imgAddress: 'https://cdn.shupian.cn/sp-pt/wap/1d02v37qg6gw000.png',
       loading: false,
       usedCount: 0, // 已使用
       notUsedCount: 0, // 未使用
@@ -163,7 +172,6 @@ export default {
       // this.$refs['myPop' + this.indexNum][0].isShow = false
       const type = this.couponType
       this.loading = true
-      console.log('type', type)
       if (type === 0) {
         this.formData.findType = 2
         this.getInitData()
@@ -198,18 +206,18 @@ export default {
       }
     },
     // 气泡组件方法
-    popOver(index) {
-      console.log(index)
-      // if(index)
-      this.$refs['myPop' + index][0].isShow = true
-      console.log(" this.$refs['myPop' + index]", this.$refs['myPop' + index])
-      this.$refs['myPop' + index][0].indexData = index
-    },
+    // popOver(index) {
+    //   console.log(index)
+    //   // if(index)
+    //   this.$refs['myPop' + index][0].isShow = true
+    //   console.log(" this.$refs['myPop' + index]", this.$refs['myPop' + index])
+    //   this.$refs['myPop' + index][0].indexData = index
+    // },
     // 关闭气泡组件
-    closeBox(data) {
-      this.indexNum = data
-      this.$refs['myPop' + this.indexNum][0].isShow = false
-    },
+    // closeBox(data) {
+    //   this.indexNum = data
+    //   this.$refs['myPop' + this.indexNum][0].isShow = false
+    // },
     // 进入详情
     goDetailPage(item) {
       if (item.useType === 1) {
@@ -239,6 +247,9 @@ export default {
 }
 .notUse {
   background-image: url('https://cdn.shupian.cn/sp-pt/wap/8ef4u05rpn8000.png');
+}
+.not_coupon_data {
+  background: #ffffff !important;
 }
 .coupon-page {
   .coupon_list {
@@ -280,13 +291,18 @@ export default {
         height: auto;
         flex: 1;
         .title {
-          height: 32px;
           font-size: 32px;
           font-family: PingFang SC;
           font-weight: bold;
           color: #222222;
           line-height: 32px;
           margin: 36px 0 23px 0;
+          word-break: break-all;
+          display: -webkit-box;
+          -webkit-line-clamp: 1;
+          -webkit-box-orient: vertical;
+          word-break: break-all;
+          overflow: hidden;
         }
         .content {
           width: 404px;
@@ -301,6 +317,7 @@ export default {
           display: -webkit-box;
           -webkit-line-clamp: 2;
           -webkit-box-orient: vertical;
+          word-break: break-all;
         }
         .date {
           font-size: 20px;
@@ -323,6 +340,22 @@ export default {
           right: 0px;
           top: 0px;
         }
+      }
+    }
+    .not_coupon {
+      width: 340px;
+      height: 340px;
+      margin: 176px auto;
+      text-align: center;
+      img {
+        width: 100%;
+        height: 100%;
+      }
+      span {
+        font-size: 30px;
+        font-family: PingFang SC;
+        font-weight: bold;
+        color: #1a1a1a;
       }
     }
   }
