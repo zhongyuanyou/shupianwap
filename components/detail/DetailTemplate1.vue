1ff605fd5e86c62ae9f8a194e7a5ace7ad0b3ffc
chenguo <chenguo1@dgg.net>
2021-04-22 14:15:10 +0800

    fix:隐藏商品精选案例模块

--- components/detail/DetailTemplate1.vue
+++ components/detail/DetailTemplate1.vue
@@ -62,7 +62,7 @@
     <TcPlanners :im-jump-query="imJumpQuery" :recommend-planner="planners" />
     <!--E 第五板块 推荐规划师-->
     <!--S  精选案例-->
-    <OrderCase></OrderCase>
+    <!-- <OrderCase></OrderCase> -->
     <!--E  精选案例-->
     <!--S 第十板块 服务详情-->
     <ServiceDetail
@@ -97,7 +97,7 @@ import { mapActions } from 'vuex'
 import Banner from '~/components/detail/Banner'
 import Title from '~/components/detail/Title1'
 import CommentBox from '~/components/detail/CommentBox'
-import OrderCase from '~/components/detail/OrderCase'
+// import OrderCase from '~/components/detail/OrderCase'
 import VouchersSelect from '~/components/detail/VouchersSelect'
 import ContainProject from '~/components/detail/ContainProject'
 import ContainContent from '~/components/detail/ContainContent'
@@ -128,7 +128,7 @@ export default {
     bottomBar,
     MyIcon,
     CommentBox,
-    OrderCase,
+    // OrderCase,
   },
   mixins: [imHandle],
   props: {
