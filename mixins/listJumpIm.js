c37c223c4037a86e9a7cd68dfbd3e206560b9a1c
zky <zhengkaiyuan@dgg.net>
2021-04-27 09:29:27 +0800

    fix:解决wap涉及小程序页面刷新报错问题

--- mixins/listJumpIm.js
+++ mixins/listJumpIm.js
@@ -32,23 +32,23 @@ export default {
         // }
         window.location.href = `${config.imBaseUrl}/index?token=${this.userInfo.token}&userId=${this.userInfo.userId}&userType=${this.userInfo.userType}`
       } else {
-        if (this.isApplets) {
-          let url = this.$route.path.split('')
-          url.splice(0, 1)
-          url = url.join('')
-          if (this.$route.query) {
-            for (const key in this.$route.query) {
-              url += `&${key}=${this.$route.query[key]}`
-            }
-          }
-          // this.uni.redirectTo({
-          //   url:
-          //     '/pages/my_son/login/wxLogin?url=' +
-          //     url +
-          //     '&firstLevel=true=true',
-          // })
-          // return
-        }
+        // if (this.isApplets) {
+        //   let url = this.$route.path.split('')
+        //   url.splice(0, 1)
+        //   url = url.join('')
+        //   if (this.$route.query) {
+        //     for (const key in this.$route.query) {
+        //       url += `&${key}=${this.$route.query[key]}`
+        //     }
+        //   }
+        //   this.uni.redirectTo({
+        //     url:
+        //       '/pages/my_son/login/wxLogin?url=' +
+        //       url +
+        //       '&firstLevel=true=true',
+        //   })
+        //   return
+        // }
         this.$router.push({
           path: '/login',
           query: {
