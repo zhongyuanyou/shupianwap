31833b1d2be483433b906a9950e7ee554d79cf3d
马靖和 <majinghe@dgg.net>
2021-05-07 16:57:16 +0800

    feat: finish comment list page.

---
+++ components/my/comment/CommentList.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="m-commonent list">
+    <div class="item">
+      <div class="item-info">
+        <sp-image round class="img" fit="cover" :src="item.avatar || ''" />
+        <div class="desc">
+          <div class="desc-name">{{ item.name || '' }}</div>
+          <div class="desc-content">{{ item.desc || '' }}</div>
+        </div>
+      </div>
+      <template v-if="item.commentFlag === 1">
+        <div class="item-button">写评价</div>
+      </template>
+      <template v-else>
+        <div class="item-button read">查看评价</div>
+      </template>
+      <div class="item-clear"></div>
+    </div>
+    <div class="placeholder"></div>
+  </div>
+</template>
+
+<script>
+import { Image } from '@chipspc/vant-dgg'
+
+export default {
+  name: 'CommentList',
+  components: {
+    [Image.name]: Image,
+  },
+  props: {
+    item: {
+      type: Object,
+      default: () => {
+        return {}
+      },
+    },
+  },
+}
+</script>
+<style lang="less" scoped>
+.m-comment.list {
+  .item {
+    padding: 32px 40px;
+    &-info {
+      display: flex;
+      .img {
+        width: 130px;
+        height: 130px;
+        border-radius: 0;
+        background: #f4f4f4;
+        margin-right: 24px;
+      }
+      .desc {
+        width: 516px;
+        &-name {
+          font-family: PingFangSC-Medium, PingFang SC;
+          font-weight: bold;
+          font-size: 28px;
+          color: #222222;
+          line-height: 36px;
+          text-overflow: -o-ellipsis-lastline;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          display: -webkit-box;
+          -webkit-line-clamp: 2;
+          line-clamp: 2;
+          -webkit-box-orient: vertical;
+          margin-bottom: 20px;
+        }
+        &-content {
+          height: 24px;
+          line-height: 24px;
+          font-size: 24px;
+          font-family: PingFangSC-Regular, PingFang SC;
+          font-weight: 400;
+          color: #999999;
+          white-space: nowrap;
+          text-overflow: ellipsis;
+          overflow: hidden;
+          word-break: break-all;
+        }
+      }
+    }
+    &-button {
+      margin-top: 32px;
+      padding: 19px 41px;
+      font-size: 26px;
+      color: #4974f5;
+      border-radius: 8px;
+      border: 1px solid #4974f5;
+      float: right;
+      &.read {
+        border-radius: 6px;
+        border: 1px solid #cccccc;
+        color: #cccccc;
+      }
+    }
+    &-clear {
+      clear: both;
+    }
+  }
+  .placeholder {
+    width: 100%;
+    height: 20px;
+    background: #f5f5f5;
+  }
+}
+</style>
--- pages/my/comment/index.vue
+++ pages/my/comment/index.vue
@@ -1,11 +1,253 @@
 <template>
-  <div></div>
+  <div class="m-comment list">
+    <Header title="评价中心" :fixed="true" />
+    <sp-tabs v-model="active" @change="changeTab">
+      <sp-tab title="待评价">
+        <sp-list
+          v-model="loading"
+          :finished="finished"
+          finished-text="没有更多了"
+          class="list"
+          :error.sync="error"
+          error-text="请求失败，点击重新加载"
+          @load="onLoad"
+        >
+          <comment-list
+            v-for="(item, index) in orderList"
+            :key="index"
+            :item="item"
+          />
+        </sp-list>
+      </sp-tab>
+      <sp-tab title="已评价">
+        <sp-list
+          v-model="loading"
+          :finished="finished"
+          finished-text="没有更多了"
+          class="list"
+          :error.sync="error"
+          error-text="请求失败，点击重新加载"
+          @load="onLoad"
+        >
+          <comment-list
+            v-for="(item, index) in orderList"
+            :key="index"
+            :item="item"
+        /></sp-list>
+      </sp-tab>
+    </sp-tabs>
+  </div>
 </template>
 
 <script>
+import { Tab, Tabs, List } from '@chipspc/vant-dgg'
+import Header from '@/components/common/head/header'
+import CommentList from '@/components/my/comment/CommentList'
+
+// mock data
+// commentFlag: 1 已评价; commentFlag: 0 未评价
+const mockOrderListFlag1 = [
+  {
+    avatar: 'https://dchipscommon.dgg188.cn/img/bg.1e53fbc6.png',
+    name:
+      '四川省武侯区有限公司代办注册服务四川省武侯区有限公司代办注册服务四川省武侯区有限公司代办注册服务',
+    desc:
+      '武侯区;无注册地址;任教申请税务武侯区;无注册地址;任教申请税务武侯区;无注册地址;任教申请税务',
+    commentFlag: 1,
+  },
+  {
+    avatar: 'https://dchipscommon.dgg188.cn/img/bg.1e53fbc6.png',
+    name: '四川省武侯区有限公司测试数据',
+    desc: '青羊区;无注册地址;任教申请税务',
+    commentFlag: 1,
+  },
+  {
+    avatar: 'https://dchipscommon.dgg188.cn/img/bg.1e53fbc6.png',
+    name: '四川省武侯区有限公司代办注册服务',
+    desc: '武侯区;无注册地址;任教申请税务',
+    commentFlag: 1,
+  },
+  {
+    avatar: 'https://dchipscommon.dgg188.cn/img/bg.1e53fbc6.png',
+    name: '四川省武侯区有限公司测试数据',
+    desc: '青羊区;无注册地址;任教申请税务',
+    commentFlag: 1,
+  },
+  {
+    avatar: 'https://dchipscommon.dgg188.cn/img/bg.1e53fbc6.png',
+    name: '四川省武侯区有限公司代办注册服务',
+    desc: '武侯区;无注册地址;任教申请税务',
+    commentFlag: 1,
+  },
+  {
+    avatar: 'https://dchipscommon.dgg188.cn/img/bg.1e53fbc6.png',
+    name: '四川省武侯区有限公司测试数据',
+    desc: '青羊区;无注册地址;任教申请税务',
+    commentFlag: 1,
+  },
+  {
+    avatar: 'https://dchipscommon.dgg188.cn/img/bg.1e53fbc6.png',
+    name: '四川省武侯区有限公司代办注册服务',
+    desc: '武侯区;无注册地址;任教申请税务',
+    commentFlag: 1,
+  },
+  {
+    avatar: 'https://dchipscommon.dgg188.cn/img/bg.1e53fbc6.png',
+    name: '四川省武侯区有限公司测试数据',
+    desc: '青羊区;无注册地址;任教申请税务',
+    commentFlag: 1,
+  },
+  {
+    avatar: 'https://dchipscommon.dgg188.cn/img/bg.1e53fbc6.png',
+    name: '四川省武侯区有限公司代办注册服务',
+    desc: '武侯区;无注册地址;任教申请税务',
+    commentFlag: 1,
+  },
+  {
+    avatar: 'https://dchipscommon.dgg188.cn/img/bg.1e53fbc6.png',
+    name: '四川省武侯区有限公司测试数据',
+    desc: '青羊区;无注册地址;任教申请税务',
+    commentFlag: 1,
+  },
+]
+
+const mockOrderListFlag0 = [
+  {
+    avatar: 'https://dchipscommon.dgg188.cn/img/bg.1e53fbc6.png',
+    name: '四川省武侯区有限公司代办注册服务',
+    desc: '武侯区;无注册地址;任教申请税务',
+    commentFlag: 0,
+  },
+  {
+    avatar: 'https://dchipscommon.dgg188.cn/img/bg.1e53fbc6.png',
+    name: '四川省武侯区有限公司测试数据',
+    desc: '青羊区;无注册地址;任教申请税务',
+    commentFlag: 0,
+  },
+  {
+    avatar: 'https://dchipscommon.dgg188.cn/img/bg.1e53fbc6.png',
+    name: '四川省武侯区有限公司代办注册服务',
+    desc: '武侯区;无注册地址;任教申请税务',
+    commentFlag: 0,
+  },
+  {
+    avatar: 'https://dchipscommon.dgg188.cn/img/bg.1e53fbc6.png',
+    name: '四川省武侯区有限公司测试数据',
+    desc: '青羊区;无注册地址;任教申请税务',
+    commentFlag: 0,
+  },
+  {
+    avatar: 'https://dchipscommon.dgg188.cn/img/bg.1e53fbc6.png',
+    name: '四川省武侯区有限公司代办注册服务',
+    desc: '武侯区;无注册地址;任教申请税务',
+    commentFlag: 0,
+  },
+  {
+    avatar: 'https://dchipscommon.dgg188.cn/img/bg.1e53fbc6.png',
+    name: '四川省武侯区有限公司测试数据',
+    desc: '青羊区;无注册地址;任教申请税务',
+    commentFlag: 0,
+  },
+  {
+    avatar: 'https://dchipscommon.dgg188.cn/img/bg.1e53fbc6.png',
+    name: '四川省武侯区有限公司代办注册服务',
+    desc: '武侯区;无注册地址;任教申请税务',
+    commentFlag: 0,
+  },
+  {
+    avatar: 'https://dchipscommon.dgg188.cn/img/bg.1e53fbc6.png',
+    name: '四川省武侯区有限公司测试数据',
+    desc: '青羊区;无注册地址;任教申请税务',
+    commentFlag: 0,
+  },
+  {
+    avatar: 'https://dchipscommon.dgg188.cn/img/bg.1e53fbc6.png',
+    name: '四川省武侯区有限公司代办注册服务',
+    desc: '武侯区;无注册地址;任教申请税务',
+    commentFlag: 0,
+  },
+  {
+    avatar: 'https://dchipscommon.dgg188.cn/img/bg.1e53fbc6.png',
+    name: '四川省武侯区有限公司测试数据',
+    desc: '青羊区;无注册地址;任教申请税务',
+    commentFlag: 0,
+  },
+]
+
 export default {
   name: 'Comment',
+  components: {
+    Header,
+    [Tab.name]: Tab,
+    [Tabs.name]: Tabs,
+    [List.name]: List,
+    CommentList,
+  },
+  data() {
+    return {
+      active: 0, // 索引值: 0|待评价 1|已评价
+      error: false,
+      loading: false,
+      finished: false,
+      page: 1,
+      limit: 15,
+      orderList: [], // 订单列表
+    }
+  },
+  methods: {
+    changeTab() {
+      this.init()
+      this.onLoad()
+    },
+    init() {
+      this.orderList = []
+      this.page = 1
+      this.error = false
+      this.finished = false
+      this.loading = true
+    },
+    onLoad() {
+      this.getOrderListApi()
+    },
+    getOrderListApi() {
+      this.orderList = mockOrderListFlag1
+      this.loading = false
+      if (this.active === 1) {
+        this.orderList = mockOrderListFlag0
+      }
+    },
+  },
 }
 </script>
 
-<style></style>
+<style lang="less" scoped>
+.m-comment.list {
+  ::v-deep.sp-tabs {
+    .sp-tabs__wrap {
+      height: 72px;
+      padding-bottom: 8px;
+      border-bottom: 1px solid #f4f4f4;
+      .sp-tab {
+        font-size: 30px;
+        font-family: PingFangSC-Regular, PingFang SC;
+        font-weight: 400;
+        color: #999999;
+        line-height: 30px;
+      }
+      .sp-tab--active {
+        color: #222222;
+        font-weight: bold;
+      }
+      .sp-tab__text--ellipsis {
+        overflow: unset;
+      }
+      .sp-tabs__line {
+        width: 28px;
+        height: 6px;
+        background: #4974f5;
+        border-radius: 3px;
+      }
+    }
+  }
+}
+</style>
