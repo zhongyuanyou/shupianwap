7c4bb6ba960769df46443bed4bd5cd6c6f7d22a5
马靖和 <majinghe@dgg.net>
2021-05-14 14:07:51 +0800

    feat: 处理必懂接口

--- api/evaluate.js
+++ api/evaluate.js
@@ -1,7 +1,6 @@
 'use strict'
 
-// import { CHIPS_WAP_BASE_URL } from '../config/constant'
-const CHIPS_WAP_BASE_URL = 'http://172.16.133.115:7001/service'
+import { CHIPS_WAP_BASE_URL } from '../config/constant'
 
 const evaluateApi = {
   detail: `${CHIPS_WAP_BASE_URL}/yk/evaluate/v1/get_evaluate_detail.do`, // 首页首屏服务端初始化请求数据
