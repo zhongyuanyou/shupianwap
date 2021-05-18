82a78867716f5f8e6b853e4ad16cf25b64473793
马靖和 <majinghe@dgg.net>
2021-05-18 10:32:36 +0800

    feat: 联调创作中心

--- pages/known/createCenter/index.vue
+++ pages/known/createCenter/index.vue
@@ -12,6 +12,46 @@
           error-text="请求失败，点击重新加载"
           @load="onLoad"
         >
+          <div
+            v-for="(itemItem, indexIndex) in list"
+            :key="indexIndex"
+            class="item-wrapper"
+            @click="link(itemItem)"
+          >
+            <div class="placeholder"></div>
+            <div class="item">
+              <div class="tile">
+                <span
+                  class="tile-flag"
+                  :class="[
+                    itemItem.status === 0
+                      ? 'z-pending'
+                      : itemItem.status === 1
+                      ? 'z-approved'
+                      : 'z-cancel',
+                  ]"
+                  >{{ itemItem.status | fliterStatus }}</span
+                ><span class="tile-txt">&nbsp;{{ itemItem.title }}</span>
+              </div>
+              <div class="content">
+                <div class="content-txt">
+                  {{ itemItem.contentText }}
+                </div>
+                <sp-image
+                  v-if="itemItem.contentImageUrl"
+                  class="content-img"
+                  fit="cover"
+                  :src="itemItem.contentImageUrl.split(',')[0]"
+                />
+              </div>
+              <div class="desc">
+                <div class="desc-tip">
+                  {{ itemItem.answerCount || 0 }} 回答 ·
+                  {{ itemItem.collectCount || 0 }} 收藏
+                </div>
+              </div>
+            </div>
+          </div>
         </sp-list>
       </sp-tab>
     </sp-tabs>
@@ -33,6 +73,12 @@ export default {
     [List.name]: List,
     [Image.name]: Image,
   },
+  filters: {
+    fliterStatus(val) {
+      const txts = ['审核中', '已通过', '未通过']
+      return txts[val]
+    },
+  },
   data() {
     return {
       error: false,
@@ -42,7 +88,7 @@ export default {
       limit: 15,
       list: [],
       active: 0,
-      activeMapping: ['', '1', '0', '2'], // status: 1 | 审核通过; 2 | 拒绝; 0 | 待审核, 如果不传status,则默认返回所有数据
+      activeMapping: [[0, 1, 2], [1], [0], [2]], // status: 1 | 审核通过; 2 | 拒绝; 0 | 待审核, 如果不传status,则默认返回所有数据
       tabList: [
         {
           tab: '全部',
@@ -99,12 +145,83 @@ export default {
         this.loading = false
       }
     },
+    link(item) {
+      // 未通过 直接跳入写页面
+      if (item.status === 2) {
+        // 问题
+        if (item.type === 1) {
+          this.$router.push({
+            path: '/known/publish/question',
+            query: {
+              id: item.id, // 问题id
+              editType: 2,
+            },
+          })
+        }
+        // 文章
+        if (item.type === 2) {
+          this.$router.push({
+            path: '/known/publish/article',
+            query: {
+              id: item.id, // 文章id
+              editType: 2,
+            },
+          })
+        } else {
+          // 回答
+          this.$router.push({
+            path: '/known/publish/answer',
+            query: {
+              id: item.id, // 传回答id
+              editType: 2,
+            },
+          })
+        }
+      } else {
+        // 通过了 跳入查看详情页面
+        // 问题
+        if (item.type === 1) {
+          this.$router.push({
+            path: '/known/detail/question',
+            query: {
+              id: item.id, // 问题id
+            },
+          })
+        }
+        // 文章
+        if (item.type === 2) {
+          this.$router.push({
+            path: '/known/detail/article',
+            query: {
+              id: item.id, // 问题id
+            },
+          })
+        } else {
+          // 回答
+          this.$router.push({
+            path: '/known/detail/answer',
+            query: {
+              id: item.id, // 传回答id
+            },
+          })
+        }
+      }
+    },
   },
 }
 </script>
 
 <style lang="less" scoped>
 .m-known.create-center {
+  .mixin-flex {
+    display: flex;
+    align-items: center;
+  }
+
+  .mixin-status(@color) {
+    color: @color;
+    border: 1px solid @color;
+  }
   ::v-deep.sp-tabs {
     .sp-tabs__wrap {
       height: 72px;
@@ -128,6 +245,69 @@ export default {
         border-radius: 3px;
       }
     }
+  }
+
+  .item {
+    padding: 40px 32px;
+    .tile {
+      position: relative;
+      margin-bottom: 20px;
+      .textOverflow(2);
+      line-height: 48px;
+      font: bold 36px @fontf-pfsc-med;
+      color: #1a1a1a;
+      &-flag {
+        position: absolute;
+        top: 8px;
+        width: 88px;
+        box-sizing: border-box;
+        font: 400 24px @fontf-pfsc-reg;
+        color: #ccc;
+        border-radius: 4px;
+        border: 1px solid #cccccc;
+        text-align: center;
+        &.z-approved {
+          .mixin-status(#ccc);
+        }
+        &.z-pending {
+          .mixin-status(#FE8C29);
+        }
+        &.z-cancel {
+          .mixin-status(#FF3B30);
+        }
+      }
+      &-txt {
+        margin-left: 88px;
+      }
+    }
+    .content {
+      .mixin-flex();
+      align-items: normal;
+      margin-bottom: 32px;
+      justify-content: space-between;
+      &-txt {
+        font: 400 30px/42px @fontf-pfsc-reg;
+        color: #555555;
+        .textOverflow(3);
+      }
+      &-img {
+        margin-left: 40px;
+        ::v-deep.sp-image__img {
+          border-radius: 12px;
+          width: 1.9rem;
+          height: 1.28rem;
+        }
+      }
+    }
+    .desc {
+      font: 400 28px/28px @fontf-pfsc-reg;
+      color: #999999;
+    }
+  }
+  .placeholder {
+    width: 100%;
+    height: 20px;
+    background: #f5f5f5;
   }
 }
 </style>
