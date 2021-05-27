9a825d9a952ed15ffb4bc5142953200efc6e083d
马靖和 <majinghe@dgg.net>
2021-05-27 19:08:50 +0800

    feat: 添加小视频页面

---
+++ pages/known/share/smallVideo/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <sp-video :vod-url="url" :sp-config="config"></sp-video>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'KnownSmallVideo',
+  data() {
+    return {
+      url: '//sf1-hscdn-tos.pstatp.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-360p.mp4',
+      config: {
+        fluid: true,
+      },
+    }
+  },
+}
+</script>
+
+<style></style>
