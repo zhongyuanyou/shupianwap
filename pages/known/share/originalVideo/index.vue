e8075da96375d84f1e3bf1c4bdbece02dbb01682
马靖和 <majinghe@dgg.net>
2021-06-01 09:47:20 +0800

    feat: 增加课程和远程视频分享页面

---
+++ pages/known/share/originalVideo/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <sp-video
+      :vod-url="url"
+      :sp-config="config"
+      :show-video="true"
+      :ignore-plugins="iplugins"
+      @errorBtnHandle="errorBtnHandle"
+    >
+      <template #shade><div class="video-shade">test shade</div></template>
+    </sp-video>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'KnownOriginalVideo',
+  data() {
+    return {
+      url: '',
+      config: {
+        height: '30vh',
+        width: '100vw',
+      },
+      iplugins: ['fullscreen'],
+    }
+  },
+  methods: {
+    errorBtnHandle(vThis) {
+      this.url =
+        '//sf1-hscdn-tos.pstatp.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-360p.mp4'
+      debugger
+      vThis.initVideo()
+    },
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.video-shade {
+  position: absolute;
+  top: 0;
+  color: #fff;
+  font-size: 24px;
+}
+</style>
