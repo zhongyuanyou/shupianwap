36f7a8ee2e450d06dff52df0103f2f0f9ff2c3c8
tangdaibing <1974776566@qq.com>
2021-06-07 14:58:25 +0800

    feat(components): 客源中心地址修改

--- api/transactionConsultation.js
+++ api/transactionConsultation.js
@@ -5,13 +5,13 @@
  */
 import {
   CRISPS_C_MIDDLE_SERVICE_API,
-  CHIPS_RESOURCE_API,
+  CHIPS_RESOURCE_URL,
 } from '../config/constant'
 
 const transactionConsApi = {
   get_sms_code: `${CRISPS_C_MIDDLE_SERVICE_API}/nk/trade/consult/get_sms_code2.do`, // 获取搜索词（推荐，热搜）
   add_consult: `${CRISPS_C_MIDDLE_SERVICE_API}/nk/trade/consult/add1.do`, // 未登录用户留言
   consult: `${CRISPS_C_MIDDLE_SERVICE_API}/nk/trade/consult/consult.do`, // 登录用户留言
-  resouse_form: `${CHIPS_RESOURCE_API}/nk/v1/business/add_allot_resource.do`, // 客源中心表单接口
+  resouse_form: `${CHIPS_RESOURCE_URL}/nk/v1/business/add_allot_resource.do`, // 客源中心表单接口
 }
 export { transactionConsApi }
