88ce1d97de4a2a84fdcdf70adef0100ec28d8dd0
xiangheng <xiangheng@dgg.net>
2021-06-23 15:28:18 +0800

    fix(ui): 发票抬头优化

--- components/order/invoice/index/HeadManagement.vue
+++ components/order/invoice/index/HeadManagement.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="history_invoice">
-    <div class="card">
+    <div v-if="companyList.length > 0" class="card">
       <div class="card_header">普通发票抬头-单位</div>
-      <div v-for="item of list" :key="item.id" class="card_content">
+      <div v-for="item of companyList" :key="item.id" class="card_content">
         <sp-cell center title="单元格" label="描述信息">
           <template #title>
             <span class="custom-title">{{ item.name }}</span>
@@ -19,66 +19,9 @@
         </sp-cell>
       </div>
     </div>
-    <div class="card">
+    <div v-if="personalList.length > 0" class="card">
       <div class="card_header">普通发票抬头-个人</div>
-      <div v-for="item of list" :key="item.id" class="card_content">
-        <sp-cell center title="单元格" label="描述信息">
-          <template #title>
-            <span class="custom-title">{{ item.name }}</span>
-            <sp-tag type="danger">默认</sp-tag>
-          </template>
-          <template #right-icon>
-            <my-icon
-              name="per_ic_addeditor"
-              size="0.28rem"
-              color="#222222"
-              class="myIcon"
-            />
-          </template>
-        </sp-cell>
-      </div>
-    </div>
-    <div class="card">
-      <div class="card_header">普通发票抬头-个人</div>
-      <div v-for="item of list" :key="item.id" class="card_content">
-        <sp-cell center title="单元格" label="描述信息">
-          <template #title>
-            <span class="custom-title">{{ item.name }}</span>
-            <sp-tag type="danger">默认</sp-tag>
-          </template>
-          <template #right-icon>
-            <my-icon
-              name="per_ic_addeditor"
-              size="0.28rem"
-              color="#222222"
-              class="myIcon"
-            />
-          </template>
-        </sp-cell>
-      </div>
-    </div>
-    <div class="card">
-      <div class="card_header">普通发票抬头-个人</div>
-      <div v-for="item of list" :key="item.id" class="card_content">
-        <sp-cell center title="单元格" label="描述信息">
-          <template #title>
-            <span class="custom-title">{{ item.name }}</span>
-            <sp-tag type="danger">默认</sp-tag>
-          </template>
-          <template #right-icon>
-            <my-icon
-              name="per_ic_addeditor"
-              size="0.28rem"
-              color="#222222"
-              class="myIcon"
-            />
-          </template>
-        </sp-cell>
-      </div>
-    </div>
-    <div class="card">
-      <div class="card_header">普通发票抬头-个人</div>
-      <div v-for="item of list" :key="item.id" class="card_content">
+      <div v-for="item of personalList" :key="item.id" class="card_content">
         <sp-cell center title="单元格" label="描述信息">
           <template #title>
             <span class="custom-title">{{ item.name }}</span>
@@ -109,16 +52,32 @@ export default {
   },
   data() {
     return {
-      list: [
+      // 个人列表
+      personalList: [
         {
           number: 1,
-          name: '11111111111111111111111111111111111111111111111111111111',
+          name: '个人名称',
           price: 22200,
           time: '2020-20-20 13:45:78',
         },
         {
           number: 2,
-          name: '11111111111111111111111111111111111111111111111111111111',
+          name: '个人名称',
+          price: 22200,
+          time: '2020-20-20 13:45:78',
+        },
+      ],
+      // 公司列表
+      companyList: [
+        {
+          number: 1,
+          name: '公司名称',
+          price: 22200,
+          time: '2020-20-20 13:45:78',
+        },
+        {
+          number: 2,
+          name: '公司名称',
           price: 22200,
           time: '2020-20-20 13:45:78',
         },
