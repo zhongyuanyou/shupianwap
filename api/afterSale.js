eb968278e6c78037d7cfcc47bbbb2f3129e33d65
zhaojun <1027138229@qq.com>
2021-07-07 18:38:28 +0800

    fix: 订单列表进售后详情参数新增

--- api/afterSale.js
+++ api/afterSale.js
@@ -1,6 +1,6 @@
 'use strict'
-import { CHIPS_PC_URL } from '../config/constant'
-// const CHIPS_PC_URL = 'http://172.16.135.188:7001/service'
+// import { CHIPS_PC_URL } from '../config/constant'
+const CHIPS_PC_URL = 'http://172.16.135.188:7001/service'
 // const CHIPS_WAP_BASE_URL = 'http://127.0.0.1:7001/service'
 const afterSaleApi = {
   list: `${CHIPS_PC_URL}/nk/orderSale/v1/list.do`, // 售后列表
