16968e7031149d5e9a9e3f41c434609389249cd4
马靖和 <majinghe@dgg.net>
2021-07-13 14:16:09 +0800

    feat: 添加store店铺api

---
+++ api/store.js
@@ -0,0 +1,10 @@
+'use strict'
+
+import { CHIPS_PC_URL } from '../config/constant'
+
+const storeApi = {
+  plannerStoreInfo: `${CHIPS_PC_URL}/yk/save/v1/add_goods.do`, // 规划师店铺详情
+  mchStoreInfo: `${CHIPS_PC_URL}/nk/store/v1/mch_store_info.do`, // 商户/团队店铺详情
+  recommendGoods: `${CHIPS_PC_URL}/nk/store/v1/recommend_goods.do`, // 获得推荐商品列表
+}
+export { storeApi }
