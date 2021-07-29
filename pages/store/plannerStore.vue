bfead1c5ce4c95a71b803350f92b98b203713423
HM-A20210617GCR\Administrator <HM-A20210617GCR\Administrator>
2021-07-29 21:27:27 +0800

    fix: 取消标签显示的设置

--- pages/store/plannerStore.vue
+++ pages/store/plannerStore.vue
@@ -163,7 +163,6 @@
                   <p class="label">
                     <span
                       v-for="(ta, taindex) in data.tags"
-                      v-show="taindex < 3"
                       :key="taindex"
                       >{{ ta }}</span
                     >
