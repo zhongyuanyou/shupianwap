4c22007b9fc4806c90e25e2ed4736c9f371925b1
xiangheng <xiangheng@dgg.net>
2021-09-14 16:44:38 +0800

    feat: 99特卖

--- components/activity/special/Card.vue
+++ components/activity/special/Card.vue
@@ -1,9 +1,6 @@
 <template>
-  <div class="body-content-items" :class="{ last: last }">
-    <div class="left-content">
-      <div v-if="endTime && endTime.sec" class="left-countdown">
-        距结束 {{ endTime.hour }}:{{ endTime.min }}:{{ endTime.sec }}
-      </div>
+  <div class="goods" :class="{ last: last }">
+    <div class="goods-image">
       <sp-image
         class="picture"
         fit="cover"
@@ -15,38 +12,14 @@
         alt="商品图片"
       />
     </div>
-    <div class="right-content">
-      <div class="goods-name">
-        <!-- <span class="rc-span">特卖</span>
-        <span class="rc-span">千万补贴</span> -->
-        {{ item.skuName }}
-      </div>
+    <div class="goods-name multiRowOverflowDot">{{ item.skuName }}</div>
 
-      <div v-if="tags.length > 0" class="rc-middle">
-        <div v-for="tag of tags" :key="tag">
-          {{ overflowDot(tag, 6) }}
-        </div>
-      </div>
-      <div class="rc-slogan">{{ item.slogan }}</div>
-      <div class="rc-bottom">
-        <div class="price-content">
-          <span
-            v-if="parsePrice(item.specialPrice) !== '面议'"
-            class="price-content-mianyi"
-          >
-            <span class="price-content-mianyi-price">
-              {{ parseFloat(item.specialPrice) }}
-            </span>
-            <span class="price-content-mianyi-price-unit">元</span>
-          </span>
-          <span v-else class="price-content-mianyi-price-unit">面议</span>
-
-          <span v-if="parsePrice(item.specialPrice) !== '面议'" class="mianyi">
-            <span>{{ parseFloat(item.skuPrice) }}</span
-            ><span>{{ '元' }}</span>
-          </span>
-        </div>
-      </div>
+    <div class="price-conainer">
+      <span v-if="parsePrice(item.specialPrice) !== '面议'">
+        <span class="price-value">{{ parseFloat(item.specialPrice) }}</span
+        ><span class="price-unit">元</span>
+      </span>
+      <span v-else class="price-unit">面议</span>
     </div>
   </div>
 </template>
@@ -60,12 +33,6 @@ export default {
     // SpIcon: Icon,
   },
   props: {
-    endTime: {
-      type: [Object, String],
-      default: () => {
-        return {}
-      },
-    },
     parsePrice: {
       type: Function,
       default() {},
@@ -82,21 +49,8 @@ export default {
       default: false,
     },
   },
-  computed: {
-    tags() {
-      const tag = this?.item?.tags ? this.item.tags.split(',') : []
-      return tag.slice(0, 2)
-    },
-  },
-  methods: {
-    overflowDot(str, num) {
-      if (str.length > 6) {
-        return str.slice(0, num) + '...'
-      } else {
-        return str
-      }
-    },
-  },
+  computed: {},
+  methods: {},
 }
 </script>
 <style lang="less" scoped>
@@ -107,155 +61,47 @@ export default {
   -webkit-line-clamp: 2; //行数
   -webkit-box-orient: vertical;
 }
-.body-content-items {
-  background: #fff;
-  display: flex;
-  justify-content: space-between;
-  height: 276px;
-  width: 100%;
-  padding: 28px 20px;
-  margin-bottom: 20px;
-  border-radius: 24px;
-}
-.body-content-items.last {
+.goods.last {
   margin-bottom: 0px;
 }
-.left-content {
-  position: relative;
-  margin-right: 32px;
-  width: 220px;
-  height: 220px;
-  flex-shrink: 0;
-  overflow: hidden;
-
+.goods {
+  font-size: 0;
+  padding: 20px 20px 24px;
+  margin-bottom: 20px;
+  box-sizing: border-box;
+  background-color: #fff;
   border-radius: 12px;
 
-  .picture {
-    height: 100%;
-    width: 100%;
+  .goods-image {
     border-radius: 12px;
+    overflow: hidden;
   }
-
-  .left-countdown {
-    white-space: nowrap;
-    font-size: 20px;
-    padding: 6px 10px;
-
-    font-weight: 400;
-    color: #ffffff;
-
-    position: absolute;
-    z-index: 1;
-    top: 8px;
-    left: 8px;
-    word-break: break-all;
-
-    background: #ec5330;
-    border-radius: 4px;
-    font-family: @fontf-pfsc-med;
-    transform-origin: left top;
-    transform: scale(0.83);
-  }
-}
-.right-content {
-  flex: 1;
-  // width: 418px;
-  display: flex;
-  align-content: flex-start;
-  position: relative;
-  height: 220px;
-  flex-direction: column;
-  overflow: hidden;
   .goods-name {
+    // height: 88px;
+    font-family: PingFangSC-Medium;
+    font-size: bold;
     font-size: 32px;
-    line-height: 42px;
-    margin-bottom: 12px;
-    font-weight: bold;
-    color: #222222;
-
-    .multiRowOverflowDot();
-    span {
-      margin-right: 4px;
-      background: #ec5330;
-      border-radius: 4px;
-      padding: 6px 8px;
-      font-size: 20px;
-      font-weight: bold;
-      color: #ffffff;
-      line-height: 20px;
-    }
-  }
-
-  .rc-middle {
-    // display: flex;
-    // align-content: flex-start;
-    margin-bottom: 16px;
-    font-size: 0;
-    div {
-      display: inline-block;
-      font-size: 20px;
-      font-weight: 400;
-      flex-shrink: 0;
-      color: #5c7499;
-      line-height: 28px;
-      padding: 0 6px;
-      background: #f0f2f5;
-      border-radius: 4px;
-      margin-right: 8px;
-      font-family: @fontf-pfsc-med;
-    }
-  }
-  .rc-slogan {
-    font-size: 22px;
     color: #222222;
     letter-spacing: 0;
-    .mixin-text-oneoverflow();
+    line-height: 44px;
+    margin: 20px 0 12px;
   }
-  .rc-bottom {
-    position: absolute;
-    bottom: 0;
-    display: flex;
-    width: 100%;
-    justify-content: space-between;
-    .price-content {
-      font-size: 0;
-      span {
-        display: inline-block;
-      }
-      .price-content-mianyi {
-        margin-right: 16px;
-      }
-      .price-content-mianyi-price {
-        font-family: @fontf-pfsc-med;
-        font-weight: bold;
-        font-size: 36px;
-        color: #ec5330;
-        letter-spacing: 0;
-        line-height: 36px;
-      }
-      .price-content-mianyi-price-unit {
-        font-family: @fontf-pfsc-med;
-
-        font-weight: bold;
-        font-size: 22px;
-        color: #ec5330;
-        letter-spacing: 0;
-        line-height: 22px;
-      }
-      .mianyi {
-        font-size: 22px;
-        color: #999999;
-        letter-spacing: 0;
-        line-height: 22px;
-        // font-family: PingFangSC-Regular, PingFang SC, PingFangSC-Medium;
-        font-family: @fontf-pfsc-reg;
-        & > span {
-          display: inline-block;
-        }
-        .bold {
-          font-weight: bold;
-        }
-      }
+  .price-conainer {
+    .price-value {
+      font-family: PingFangSC-Medium;
+      font-size: bold;
+      font-size: 36px;
+      color: #ec5330;
+      letter-spacing: 0;
+      line-height: 36px;
+    }
+    .price-unit {
+      font-family: PingFangSC-Medium;
+      font-size: bold;
+      font-size: 22px;
+      color: #ec5330;
+      letter-spacing: 0;
+      line-height: 22px;
     }
   }
 }
