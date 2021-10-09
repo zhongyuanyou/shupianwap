2ccb4e49fc8a842ecc6afcf21df582420be66a57
马靖和 <majinghe@dgg.net>
2021-10-09 09:34:50 +0800

    feat: 处理下拉收起

--- components/detail/common/ServiceDetail.vue
+++ components/detail/common/ServiceDetail.vue
@@ -3,7 +3,22 @@
     <p class="container_title">
       {{ title }}
     </p>
-    <div class="container_html" v-html="custDetail.clientDetail"></div>
+    <div
+      ref="containerHtmlRef"
+      class="container_html"
+      :style="{ height: heightContainer, overflow: overflowContainer }"
+      v-html="custDetail.clientDetail"
+    ></div>
+    <div v-if="showContainer" class="container_handle">
+      <div class="open" @click="trigger">
+        {{ triggerContainer ? '收起' : '展开' }}
+      </div>
+      <my-icon
+        :name="triggerContainer ? 'shangla' : 'xiala'"
+        size="0.24rem"
+        color="#4974F5"
+      ></my-icon>
+    </div>
   </div>
 </template>
 
@@ -26,11 +41,39 @@ export default {
   data() {
     return {
       containerLoading: true,
+      showContainer: false, // 显示内容,收起内容
+      triggerContainer: false, // 展开内容,收起内容
+      heightContainer: 'unset',
+      overflowContainer: 'unset',
     }
   },
   computed: {
     custDetail() {
       return this.detailData
+    },
+  },
+  mounted() {
+    try {
+      // 防止程序报错
+      this.$nextTick(() => {
+        const containerRefHeight = this.$refs.containerHtmlRef.offsetHeight
+        if (containerRefHeight > 200) {
+          this.showContainer = true
+          this.heightContainer = '200px'
+          this.overflowContainer = 'hidden'
+        }
+      })
+    } catch (e) {}
+  },
+  methods: {
+    trigger() {
+      this.triggerContainer = !this.triggerContainer
+      if (this.triggerContainer) {
+        this.heightContainer = this.overflowContainer = 'unset'
+        return
+      }
+      this.heightContainer = '200px'
+      this.overflowContainer = 'hidden'
     },
   },
 }
@@ -39,7 +82,7 @@ export default {
 <style lang="less" scoped>
 .container {
   background-color: #fff;
-  padding: 48px 40px 31px 40px;
+  padding: 40px;
   border-bottom: 24px solid #f8f8f8;
   &_title {
     font-size: 40px;
@@ -54,6 +97,21 @@ export default {
     color: #595959;
     ::v-deep img {
       max-width: 100%;
+    }
+  }
+  &_handle {
+    margin-top: 40px;
+    display: flex;
+    bottom: 40px;
+    align-items: center;
+    justify-content: center;
+    .open {
+      font-size: 32px;
+      line-height: 44px;
+      font-weight: bold;
+      color: #4974f5;
+      text-align: center;
+      margin-right: 18px;
     }
   }
 }
