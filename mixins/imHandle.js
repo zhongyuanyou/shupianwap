26640ba437a91dc81ac5acc5c3a383124e59c7b2
zhengting <592208416@qq.com>
2021-10-12 19:17:28 +0800

    fix: 更换创建会话发送模板消息

--- mixins/imHandle.js
+++ mixins/imHandle.js
@@ -346,77 +346,74 @@ export default {
     // mchUserId: 规划师id; text: 消息
     sendTextMessageV2(mchUserId, text) {
       const userInfo = this.$store.state.user.userInfo
-      if (userInfo) {
-        const userInfo = this.$store.state.user.userInfo
-        const userType = userInfo && userInfo.type ? userInfo.type : 'VISITOR'
-        const params = {
-          operUserType: userType,
-          imUserId: mchUserId,
-          imUserType: 'MERCHANT_USER',
-          ext: {
-            intentionType: '',
-            intentionCity: '',
-            recommendId: '',
-            recommendAttrJson: {},
-            startUserType: 'cps-app',
-          },
-        }
-        // 发送消息前先创建会话
-        this.imExample.createSession(params, (res) => {
-          if (res.code === 200) {
-            const tepMsgParams = {
-              receiver: res.data.groupId, // 会话 id
-              msgType: 'text',
-              senderName: userInfo.nickName || '访客', // 发送者昵称
-              extContent: JSON.stringify(this.$route.query), // 路由参数
-              content: {
-                text
-              },
-              msgClass: 5,
-              receiveType:res.data.groupType
-            }
-            tepMsgParams.paramJsonStr = JSON.stringify(
-              tepMsgParams.paramJsonStr
-            )
-            try {
-              this.imExample.sendMsg(tepMsgParams, (resData) => {
-                if (resData.code === 200) {
-                  // 延时1s进入IM,避免模板消息未发生完成就已进入IM
-                  this.$xToast.showLoading({ message: '正在联系规划师...' })
-                  const timer = setTimeout(() => {
-                    clearTimeout(timer)
-                    this.$xToast.hideLoading()
-                    const myInfo = localStorage.getItem('myInfo')
-                      ? JSON.parse(localStorage.getItem('myInfo'))
-                      : {}
-                    const token = this.userType ? this.token : myInfo.token
-                    const userId = this.userType ? this.userId : myInfo.imUserId
-                    const userType = this.userType || 'VISITOR'
-                    if (this.isApplets) {
-                      window.location.href = `${config.imBaseUrl}/chat?token=${token}&userId=${userId}&userType=${userType}&operUserType=${userType}&id=${res.data.groupId}&isApplets=true`
-                    } else {
-                      window.location.href = `${config.imBaseUrl}/chat?token=${token}&userId=${userId}&userType=${userType}&operUserType=${userType}&id=${res.data.groupId}`
-                    }
-                  }, 2000)
-                } else if (res.code === 5223) {
-                  this.clearUserInfoAndJumpLoging()
-                } else {
-                  this.$xToast.warning(resData.msg)
-                }
-              })
-            } catch (error) {
-              this.$xToast.error('网络出小差了，请稍后重试...' )
-            }
-          } else if (res.code === 5223) {
-            console.log('发送消息', res)
-            this.clearUserInfoAndJumpLoging()
-          } else {
-            console.log('发送消息', res)
-            this.$xToast.warning(res.msg)
-          }
-        })
-      }
-    },
+       if (userInfo) {
+         const userInfo = this.$store.state.user.userInfo
+         const userType = userInfo && userInfo.type ? userInfo.type : 'VISITOR'
+         const params = {
+           operUserType: userType,
+           imUserId: mchUserId,
+           imUserType: 'MERCHANT_USER',
+           ext: {
+             intentionType: '',
+             intentionCity: '',
+             recommendId: '',
+             recommendAttrJson: {},
+             startUserType: 'cps-app',
+           },
+         }
+         // 发送消息前先创建会话
+         this.imExample.createSession(params, (res) => {
+           if (res.code === 200) {
+             const tepMsgParams = {
+               templateId: '6045e190ba0fd10006c2343f', // 模板 id
+               receiver: res.data.groupId, // 会话 id
+               msgType: 'im_tmplate',
+               senderName: userInfo.nickName || '访客', // 发送者昵称
+               extContent: JSON.stringify(this.$route.query), // 路由参数
+               paramJsonStr: {
+                 forwardAbstract: '摘要信息',
+                 content: text,
+                 disableForward: 0,
+               },
+             }
+             tepMsgParams.paramJsonStr = JSON.stringify(
+               tepMsgParams.paramJsonStr
+             )
+             this.imExample.sendTemplateMsg(tepMsgParams, (resData) => {
+               if (resData.code === 200) {
+                 // 延时1s进入IM,避免模板消息未发生完成就已进入IM
+                 this.$xToast.showLoading({ message: '正在联系规划师...' })
+                 const timer = setTimeout(() => {
+                   clearTimeout(timer)
+                   this.$xToast.hideLoading()
+                   const myInfo = localStorage.getItem('myInfo')
+                     ? JSON.parse(localStorage.getItem('myInfo'))
+                     : {}
+                   const token = this.userType ? this.token : myInfo.token
+                   const userId = this.userType ? this.userId : myInfo.imUserId
+                   const userType = this.userType || 'VISITOR'
+                   if (this.isApplets) {
+                     window.location.href = `${config.imBaseUrl}/chat?token=${token}&userId=${userId}&userType=${userType}&operUserType=${userType}&id=${res.data.groupId}&isApplets=true`
+                   } else {
+                     window.location.href = `${config.imBaseUrl}/chat?token=${token}&userId=${userId}&userType=${userType}&operUserType=${userType}&id=${res.data.groupId}`
+                   }
+                 }, 2000)
+               } else if (res.code === 5223) {
+                 this.clearUserInfoAndJumpLoging()
+               } else {
+                 this.$xToast.warning(resData.msg)
+               }
+             })
+           } else if (res.code === 5223) {
+             console.log('发送消息', res)
+             this.clearUserInfoAndJumpLoging()
+           } else {
+             console.log('发送消息', res)
+             this.$xToast.warning(res.msg)
+           }
+         })
+       }
+     },
     /**
      * @author tangdaibing
      * @description: IM 创建与发送商品模板消息
