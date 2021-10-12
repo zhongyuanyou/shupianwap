d721e24cab713504ca0a3ba09df3785a05abcd22
马靖和 <majinghe@dgg.net>
2021-10-12 16:23:15 +0800

    feat: 处理产品详情

--- components/detail/server/Info.vue
+++ components/detail/server/Info.vue
@@ -20,21 +20,45 @@ export default {
   components: {},
   data() {
     return {
+      // 运营提供数据,对应 key 关键字
       tempAttr: [
         {
-          key: '期望期限',
+          key: [
+            'SX20210820541037',
+            'SX20210820541020',
+            'SX20210820541048',
+            'SX20210820541150',
+            'SX20210820541032',
+            'SX20210820541095',
+            'SX20210820541140',
+          ],
           text: '贷款期数',
         },
         {
-          key: '期望额度',
+          key: [
+            'SX20210820541035',
+            'SX20210820541025',
+            'SX20210820541046',
+            'SX20210820541154',
+            'SX20210820541030',
+            'SX20210820541098',
+            'SX20210820541144',
+          ],
           text: '贷款额度',
         },
         {
-          key: '月利息要求',
+          key: [
+            'SX20210820541036',
+            'SX20210820541022',
+            'SX20210820541047',
+            'SX20210820541149',
+            'SX20210820541027',
+            'SX20210820541094',
+          ],
           text: '月贷款利率',
         },
         {
-          key: '还款方式',
+          key: ['SX20210820541143'],
           text: '还款方式',
         },
       ], // 展示属性
@@ -51,7 +75,7 @@ export default {
     this.tempAttr.forEach((item) => {
       const tempItemVals = []
       attr.forEach((item1) => {
-        if (item1.attrName === item.key) {
+        if (item.key.some((n) => n === item1.attrCode)) {
           tempItemVals.push(item1.attrValName)
         }
       })
