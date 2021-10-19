1419bb3484e5900a20c52fb10f12f98a44aadb24
tangdaibing <1974776566@qq.com>
2021-10-19 16:15:42 +0800

    fix(components): 修改必懂删除内容不成功的提示

--- pages/known/detail/answer.vue
+++ pages/known/detail/answer.vue
@@ -576,7 +576,8 @@ export default {
               } else {
                 Toast.fail({
                   duration: 2000,
-                  message: '服务异常，请刷新重试！',
+                  message:
+                    res.data.error || res.message || '服务异常，请刷新重试！',
                   forbidClick: true,
                   className: 'my-toast-style',
                 })
@@ -584,6 +585,12 @@ export default {
             })
         })
         .catch((err) => {
+          Toast.fail({
+            duration: 2000,
+            message: '服务异常，请刷新重试！',
+            forbidClick: true,
+            className: 'my-toast-style',
+          })
           console.log(err)
           // on cancel
         })
